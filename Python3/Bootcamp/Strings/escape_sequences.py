a1cf5a60b6af8f51487ec14da86868333e001ab9
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-20 10:53:31 -0800

    Add print statements

--- Python3/Bootcamp/Strings/escape_sequences.py
+++ Python3/Bootcamp/Strings/escape_sequences.py
@@ -1,12 +1,13 @@
  # Set the message variable equal to any string containing a new-line escape sequence
 message = "hello\nthere"
-
+print(messsage)
 
 # Add a string to the mountains variable that when printed results in: /\/\/\
 # You will need to use an escape sequence more than once!
 mountains = "/\\/\\/\\"
-
+print(mountains)
 
 # Set the quotation variable to any string that contains an escaped double quotation mark
 
 quotation = "She said \"how funny\""
+print(qoutation)
