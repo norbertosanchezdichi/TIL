03c69f66bd7d8665357ee57c9b37d20ee2edc1c1
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-19 22:19:43 -0800

    Initial commit

---
+++ Python3/NumbersAndMath/numbers_and_math.py
@@ -0,0 +1,10 @@
+// PEMDAS is followed for calculations.
+
+// Exponentiation
+print("2^2 =" + str(2 ** 2))
+
+// Division always returns a float
+print ("1 / 2 = " + str(1 / 2))
+
+// For integer division, use double forward-slashes
+print ("1 // 2 = " + str(1 // 2))
