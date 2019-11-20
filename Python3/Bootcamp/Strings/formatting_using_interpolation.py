e7bc5bd9050cd130e9c6e57dd4e0b1b8d698d6bd
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-20 11:23:37 -0800

    Add name of interpolation method

--- Python3/Bootcamp/Strings/formatting_using_interpolation.py
+++ Python3/Bootcamp/Strings/formatting_using_interpolation.py
@@ -1,12 +1,10 @@
 first = "Norberto"
 last = "Sánchez-Dichi"
 
-
-
-# Python 3.5 and below
+# Python 3.5 and below using .format()
 formatted_old = "First Name: {}, Last Name: {}".format(first, last)
 print(formatted_old)
 
-# Python 3.6 and up
+# Python 3.6 and up using f-String
 formatted_new = f"First Name: {first}, Last Name: {last}"
 print(formatted_new)
