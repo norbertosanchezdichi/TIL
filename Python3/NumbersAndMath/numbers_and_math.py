667e0852a9765032067d8b4f14eea98208fde003
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-19 22:29:58 -0800

    Add modulus example

--- Python3/NumbersAndMath/numbers_and_math.py
+++ Python3/NumbersAndMath/numbers_and_math.py
@@ -8,3 +8,7 @@ print ("1 / 2 = " + str(1 / 2))
 
 # For integer division, use double forward-slashes
 print ("1 // 2 = " + str(1 // 2))
+
+# Modulo is like division except it returns the remainder
+
+print ("10 % 3 = " + str(10 % 3))
