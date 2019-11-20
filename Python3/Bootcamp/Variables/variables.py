0cfeae514a4649371b2561c7fb1cfa7f3b71cee8
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-20 10:53:16 -0800

    Add print statements

--- Python3/Bootcamp/Variables/variables.py
+++ Python3/Bootcamp/Variables/variables.py
@@ -1,11 +1,15 @@
 # Define a variable named city and set it equal to any string
 city = 'San Jose'
+print(type(city))
 
 # Define a variable named price and set it equal to any float
 price = 9.99
+print(type(price))
 
 # Define a variable named high_score and set it equal to any int
 high_score = 10
+print(type(high_score))
 
 # Define a variable named is_having_fun and set it to a Boolean value
 is_having_fun = True
+print(type(is_having_fun))
