1d52f1c8f82faaf59c1183b814d28d587653bf5a
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-20 11:15:41 -0800

    Initial commit

---
+++ Python3/Bootcamp/Strings/formatting.py
@@ -0,0 +1,10 @@
+first = "Norberto"
+last = "Sánchez-Dichi"
+
+# Python 3.5 and below
+formatted_old = "First Name: {}, Last Name: {}".format(first, last)
+print(formatted_old)
+
+# Python 3.6 and up
+formatted_new = f"First Name: {first}, Last Name: {last}"
+print(formatted_new)
