ec8453f6be953fbee295211946a889de470e1e86
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-21 22:09:39 -0800

    Optimize logic

--- Python3/Bootcamp/Programs/Bouncer/bouncer.py
+++ Python3/Bootcamp/Programs/Bouncer/bouncer.py
@@ -2,11 +2,11 @@ age = input('How old are you?')
 
 if age:
 	age = int(age)
-
-	if age >= 18 and age < 21:
-		print('You can enter but you need a wristband!')
-	elif age >= 21:
+	
+	if age >= 21:
 		print('You can enter and can drink!')
+	elif age >= 18:
+		print('You can enter but you need a wristband!')
 	else:
 		print('You can\'t come in, little one! :[')
 else:
