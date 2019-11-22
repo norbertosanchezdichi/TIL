5fd7fa2cfe575ad25248bc7670a67986fd37a4a4
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-21 20:38:44 -0800

    Print out content of variables

--- Python3/Bootcamp/ConditionalLogic/is_vs_==.py
+++ Python3/Bootcamp/ConditionalLogic/is_vs_==.py
@@ -1,11 +1,13 @@
 x = 13
 y = 13
 
+print(f'x = {x}, y = {y}')
 print(f'x == y: {x == y}')
 print(f'x is y: {x is y}')
 
 a = [1, 2, 3]
 b = [1, 2, 3]
 
+print(f'a = {a}, b = {b}')
 print(f'a == b: {a == b}')
 print(f'a is b: {a is b}')
