0e4d72a1770024b7a54a9c80eb1e9a268680e18b
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-23 20:00:54 -0800

    Initial commit

---
+++ Python3/Bootcamp/Programs/CallingInSick/calling_in_sick.py
@@ -0,0 +1,15 @@
+from random import choice, randint
+
+# randomly assigns values to these four variables
+actually_sick = choice([True, False])
+kinda_sick = choice([True, False])
+hate_your_job = choice([True, False])
+sick_days = randint(0, 10)
+
+calling_in_sick = False
+
+if ((actually_sick is True) and (sick_days != 0)) or ((kinda_sick is True) and (hate_your_job is True) and (sick_days != 0)):
+    
+    calling_in_sick = True
+    
+print(f'Calling in sick is {calling_in_sick}')
