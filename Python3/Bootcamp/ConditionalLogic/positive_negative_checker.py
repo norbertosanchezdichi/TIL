f472ad297ce65ac612d0e754c605cd08a238bca2
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-23 19:42:47 -0800

    Optimize logic

--- Python3/Bootcamp/ConditionalLogic/positive_negative_checker.py
+++ Python3/Bootcamp/ConditionalLogic/positive_negative_checker.py
@@ -6,22 +6,17 @@ y = randint(-100, 100)                               #|    /
 while y == 0:  # make sure y isn't zero              #|   /
     y = randint(-100, 100)                           #|  /
 
-if x > 0:
+if x > 0 and y > 0:
+
+    print("both positive")
     
-    if y > 0:
+elif x < 0 and y < 0:
+
+    print("both negative")
     
-        print("both positive")
+elif x > 0 and y < 0:
 
-    else: 
-        
-        print("x is positive and y is negative")
-        
-elif x < 0:
+    print("x is positive and y is negative")
+else:
 
-    if y > 0:
-        
-        print("y is positive and x is negative")
-
-    elif y < 0:
-
-        print("both negative")
+    print("y is positive and x is negative")
