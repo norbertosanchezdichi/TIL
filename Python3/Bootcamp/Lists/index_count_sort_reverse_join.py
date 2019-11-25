6c3903c2a9f643ed8aba4298fee6e8a653ed61c3
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-24 22:34:11 -0800

    Add reverse method

--- Python3/Bootcamp/Lists/index_count_sort_reverse_join.py
+++ Python3/Bootcamp/Lists/index_count_sort_reverse_join.py
@@ -6,3 +6,6 @@ print(f'Index of second 1 is {numbers.index(1, 3)}')
 print(f'Index of last 3 is {numbers.index(3, len(numbers) - 3, len(numbers))}')
 
 print(f'Number of times 28 exists in list: {numbers.count(28)}')
+
+numbers_reversed = numbers.reverse()
+print(numbers_reversed)
