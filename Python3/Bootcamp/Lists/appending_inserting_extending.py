81c4d0049aeba8f79ba99ff748937065fc6bb3a0
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-24 22:10:38 -0800

    Fix SyntaxError

--- Python3/Bootcamp/Lists/appending_inserting_extending.py
+++ Python3/Bootcamp/Lists/appending_inserting_extending.py
@@ -4,7 +4,7 @@ print(list)
 list.append('THE FIRST')
 print(list)
 
-list.insert(0, ['WAIT...', 'WHERE AM I?']
+list.insert(0, ['WAIT...', 'WHERE AM I?'])
 print(list)
 
 list.extend(['NICE DUDES', 'FINISH LAST'])
