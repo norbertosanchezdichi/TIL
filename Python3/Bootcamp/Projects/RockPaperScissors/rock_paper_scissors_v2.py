6768a29f9c73d1f0a6f84ea2850b56a8356cce51
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-24 17:08:26 -0800

    Initial commit

---
+++ Python3/Bootcamp/Projects/RockPaperScissors/rock_paper_scissors_v2.py
@@ -0,0 +1,20 @@
+import random
+
+print('Rock...')
+print('Paper...')
+print('Scissors...')
+
+player1 = input('Player 1, make your move: ').lower()
+
+player2 = input('Player 2, make your move: ')
+
+if player1 == player2:
+	print('Its a TIE!')
+elif player1 == 'rock' and player2 == 'scissors':
+	print('Player 1 WINS!')
+elif player1 == 'paper' and player2 == 'rock':
+	print('Player 1 WINS!')
+elif player1 == 'scissors' and player2 == 'paper':
+	print('Player 1 WINS!')
+else:
+	print('Player 2 WINS!')
