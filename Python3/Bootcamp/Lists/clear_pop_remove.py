985340f562fcb1376da3ae817bb8c30aa7c93c9d
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-24 22:18:39 -0800

    Add missing apostrophe.

--- Python3/Bootcamp/Lists/clear_pop_remove.py
+++ Python3/Bootcamp/Lists/clear_pop_remove.py
@@ -1,9 +1,9 @@
-list = [10, 'heh', 'i\'m a dude', 'he\s a dude', 'she\'s a dude']
+list = [10, 'heh', 'i\'m a dude', 'he\'s a dude', 'she\'s a dude']
 print(list)
 
 last_item = list.pop()
 print(list)
-print(f'Last item {last_item} has been popped.')
+print(f'Last item \'{last_item}\' has been popped.')
 
 list.remove(10)
 print(list)
