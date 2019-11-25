2d792713244ece6f5eb0b8803ba106836dcb4c14
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-24 16:34:27 -0800

    Print period separator 100 times

--- Python3/Bootcamp/Projects/RockPaperScissors/rock_paper_scissors.py
+++ Python3/Bootcamp/Projects/RockPaperScissors/rock_paper_scissors.py
@@ -3,7 +3,7 @@ print('Paper...')
 print('Scissors...')
 
 player1 = input('Player 1, make your move: ')
-print('.................' * 10)
+print('.................' * 100)
 player2 = input('Player 2, make your move: ')
 
 if player1 == player2:
