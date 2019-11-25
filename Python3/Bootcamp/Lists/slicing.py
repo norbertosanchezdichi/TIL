8d1c02b7802d01905977689bc7778c5733260899
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-25 09:55:31 -0800

    Initial commit

---
+++ Python3/Bootcamp/Lists/slicing.py
@@ -0,0 +1,19 @@
+list1 = [range(1, 5]
+print(list1)
+
+list2 = list1[1:]
+print(list2)
+
+list3 = list1[3:]
+print(list3)
+
+list4 = list1[-1:]
+print(list4)
+
+list5 = list1[-3:]
+print(list5)[
+
+list6 = list1[:]
+print(list6)
+print(f'list6 is list1 : {list6 is list1}')
+print(f'list6 == list1: {list6 == list1}')
