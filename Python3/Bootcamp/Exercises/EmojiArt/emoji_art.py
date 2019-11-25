5340dd58a7ab290561978945dbcd3ef2b1cc5269
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-24 19:26:15 -0800

    Add method without string multiplication

--- Python3/Bootcamp/Exercises/EmojiArt/emoji_art.py
+++ Python3/Bootcamp/Exercises/EmojiArt/emoji_art.py
@@ -7,3 +7,15 @@ while number < 11:
 
 	print('\U0001f600' * number)
 	number += 1
+
+for number in range(1,11):
+	
+	count = 1
+	smileys = ""
+	
+	while count < number:
+	
+		smileys += '\U0001f600'
+		count += 1
+	
+	print(smileys)
