82312f95e6cbe0d23638c5b17117e7802870db40
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-24 22:32:29 -0800

    Add count method

--- Python3/Bootcamp/Lists/index_count_sort_reverse_join.py
+++ Python3/Bootcamp/Lists/index_count_sort_reverse_join.py
@@ -4,3 +4,5 @@ print(numbers)
 print(f'Index of 5 is {numbers.index(5)}')
 print(f'Index of second 1 is {numbers.index(1, 3)}')
 print(f'Index of last 3 is {numbers.index(3, len(numbers) - 3, len(numbers))}')
+
+print(f'Number of times 28 exists in list: {numbers.count(28)}')
