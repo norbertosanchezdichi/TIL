da9f6c4144ad883542270d8d61497dfa66617235
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-24 21:29:34 -0800

    Use un-packing operator to create list from range object

--- Python3/Bootcamp/Lists/creating_lists.py
+++ Python3/Bootcamp/Lists/creating_lists.py
@@ -2,7 +2,5 @@ list1 = ['a', 234, True, 43.23]
 print(list1)
 print(f'Length of list1 is {len(list1)}')
 
-list2 = range(12, 28, 3)
+list2 = [*range(12, 28, 3)]
 print(list2)
-for item in list2:
-    print(item)
