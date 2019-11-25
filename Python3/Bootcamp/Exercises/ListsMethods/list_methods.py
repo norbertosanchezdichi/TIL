dbc219e68cd2981511ebe77978e94abb5ca0eff7
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-25 09:50:15 -0800

    Initial commit

---
+++ Python3/Bootcamp/Exercises/ListsMethods/list_methods.py
@@ -0,0 +1,14 @@
+instructors = []
+print(instructors)
+
+instructors.extend(['Colt', 'Blue', 'Lisa'])
+print(instructors)
+
+instructors.pop()
+print(instructors)
+
+instructors.pop(0)
+print(instructors)
+
+instructors.insert(0, 'Done')
+print(instructors)
