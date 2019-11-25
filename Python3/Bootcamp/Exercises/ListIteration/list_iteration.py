221937504d61536b298ea0dc6c55c11115eb110f
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-24 21:50:54 -0800

    Initial commit

---
+++ Python3/Bootcamp/Exercises/ListIteration/list_iteration.py
@@ -0,0 +1,10 @@
+sounds = ["super", "cali", "fragil", "istic", "expi", "ali", "docious"]
+
+result = ''
+
+for fragment in sounds:
+    
+    result += fragment
+    
+result = result.upper()
+print(result)
