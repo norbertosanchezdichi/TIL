4355931b8d110110a1b1c4ba2455c51d0b159efd
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-24 21:24:22 -0800

    Iterate through list2 to print contents

--- Python3/Bootcamp/Lists/creating_lists.py
+++ Python3/Bootcamp/Lists/creating_lists.py
@@ -1,6 +1,8 @@
 list1 = ['a', 234, True, 43.23]
 print(list1)
-print(f'Lenght of list1 is {len(list1)}')
+print(f'Length of list1 is {len(list1)}')
 
 list2 = range(12, 28, 3)
 print(list2)
+for item in list2:
+    print(item)
