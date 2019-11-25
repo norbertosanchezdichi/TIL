d7d8b2bd8957cc6fa2d357d6c5a5ec76fb5807ea
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-25 15:14:06 -0800

    Add print statements showing how slicing is being applied

--- Python3/Bootcamp/Lists/slicing.py
+++ Python3/Bootcamp/Lists/slicing.py
@@ -1,32 +1,32 @@
-# Using start parameter
+print('Using start parameter')
 
 list1 = list(range(1, 5))
-print(list1)
+print(f'list1 = {list1}')
 
 list2 = list1[1:]
-print(list2)
+print(f'list1[1:] = {list2}')
 
 list3 = list1[3:]
-print(list3)
+print(f'list1[3:] = {list3}')
 
 list4 = list1[-1:]
-print(list4)
+print(f'list1[-1:] = {list4}')
 
 list5 = list1[-3:]
-print(list5)
+print(f'list1[-3:] = {list5}')
 
 list6 = list1[:]
 print(list6)
 print(f'list6 is list1 : {list6 is list1}')
 print(f'list6 == list1: {list6 == list1}')
 
-# Using end parameter
+print('Using end parameter')
 
 list7 = list1[:2]
-print(list7)
+print(f'list1[:2] = {list7}')
 
 list8 = list1[2:4]
-print(list8)
+print(f'list1[2:4] = {list8}')
 
 list9 = list1[1:-1]
-print(list9)
+print(f'list1[1:-1] = {list9}')
