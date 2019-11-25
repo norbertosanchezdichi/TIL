39863d65644266aefbd1fca5f63881636c0634b4
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-24 19:43:14 -0800

    Add break statement after more than 3 times have been screamed

--- Python3/Bootcamp/Exercises/ScreamingRepeating/screaming_repeating.py
+++ Python3/Bootcamp/Exercises/ScreamingRepeating/screaming_repeating.py
@@ -2,4 +2,11 @@ number_of_times = input('How many times do I have to tell you? ')
 number_of_times = int(number_of_times)
 
 for time in range(number_of_times):
+	
 	print(f'time {time + 1}: CLEAN UP YOUR ROOM!')
+	
+	if time > 3:
+	
+		print('DO YOU EVEN LISTEN ANYMORE?')
+		
+		break
