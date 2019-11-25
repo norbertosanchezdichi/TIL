3cbf66954ce8c3526ec76ceb487d0f296fd2914b
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-24 22:27:34 -0800

    Fix SyntaxError

--- Python3/Bootcamp/Lists/index_count_sort_reverse_join.py
+++ Python3/Bootcamp/Lists/index_count_sort_reverse_join.py
@@ -2,5 +2,5 @@ numbers = [3, 5, 1, 1, 7, 10, 3]
 print(numbers)
 
 print(f'Index of 5 is {numbers.index(5)}')
-print(f'Index of second 1 is {numbers.index(1, 3)')
-print(f'Index of last 3 is {numbers.index(3, len(numbers) - 2, len(numbers) - 1)
+print(f'Index of second 1 is {numbers.index(1, 3)}')
+print(f'Index of last 3 is {numbers.index(3, len(numbers) - 3, len(numbers - 1))}')
