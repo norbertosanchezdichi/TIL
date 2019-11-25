0e47a2c41e8df954bb85bd2d3fe1e1d7b1d36ebf
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-24 22:24:52 -0800

    Initial commit

---
+++ Python3/Bootcamp/Lists/index_count_sort_reverse_join.py
@@ -0,0 +1,6 @@
+numbers = [3, 5, 1, 1, 7, 10, 3]
+print(numbers)
+
+print(f'Index of 5 is {numbers.index(5)}')
+print(f'Index of second 1 is {numbers.index(1, 3)')
+print(f'Index of last 3 is {numbers.index(3, len(numbers) - 2, len(numbers) - 1)
