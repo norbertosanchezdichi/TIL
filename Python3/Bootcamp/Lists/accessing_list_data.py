8040419ef0924099b75651821f274757e2025759
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-24 21:38:32 -0800

    Initial commit

---
+++ Python3/Bootcamp/Lists/accessing_list_data.py
@@ -0,0 +1,7 @@
+people = ["Hanna","Louisa","Claudia", "Angela","Geoffrey", "aparna"]
+print(people)
+
+people[0] = "Hannah"
+people[len(people) - 2] = "Jeffrey"
+people[len(people) - 1] = "Aparna"
+print(people)
