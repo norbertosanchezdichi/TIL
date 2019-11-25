55670b0d97d0ac75f5915a0d460eb305ead8ed92
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-24 22:16:43 -0800

    Initial commit

---
+++ Python3/Bootcamp/Lists/clear_pop_remove.py
@@ -0,0 +1,12 @@
+list = [10, 'heh', 'i\'m a dude', 'he\s a dude', 'she\'s a dude']
+print(list)
+
+last_item = list.pop()
+print(list)
+print(f'Last item {last_item} has been popped.')
+
+list.remove(10)
+print(list)
+
+list.clear()
+print(list)
