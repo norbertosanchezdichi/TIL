f0e573b0f8951f6f414ab34cbfe43d6149a41c34
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-24 19:03:29 -0800

    Initial commit

---
+++ Python3/Bootcamp/Exercises/ScreamingRepeating/screaming_repeating.py
@@ -0,0 +1,5 @@
+number_of_times = input('HOw many times do I have to tell you? ')
+number_of_times = int(number_of_times)
+
+for time in range(times):
+	print(f'time {time + 1}: CLEAN UP YOUR ROOM!')
