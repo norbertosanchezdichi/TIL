ca5a24ed9054f7a4dd11212fd86389efffe9c1a9
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-24 22:29:24 -0800

    Fix TypeError

--- Python3/Bootcamp/Lists/index_count_sort_reverse_join.py
+++ Python3/Bootcamp/Lists/index_count_sort_reverse_join.py
@@ -3,4 +3,4 @@ print(numbers)
 
 print(f'Index of 5 is {numbers.index(5)}')
 print(f'Index of second 1 is {numbers.index(1, 3)}')
-print(f'Index of last 3 is {numbers.index(3, len(numbers) - 3, len(numbers - 1))}')
+print(f'Index of last 3 is {numbers.index(3, len(numbers) - 3, len(numbers) - 1)}')
