4c206ec607607f855a4be70d3213653d67cf13a3
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-25 16:36:29 -0800

    Use 'vowels' variable

--- Python3/Bootcamp/Lists/comprehension_with_conditional_logic.py
+++ Python3/Bootcamp/Lists/comprehension_with_conditional_logic.py
@@ -4,5 +4,6 @@ print(f'{[number for number in numbers if number % 2 == 0] =}')
 print(f'{[number for number in numbers if number % 2 != 0] =}')
 print(f'{[number * 2 if number % 2 == 0 else number / 2 for number in numbers] =}')
 
-with_vowels = "This is so much fun!"
-print(f'{"".join(char in for char in with_vowels if char not in 'aeiou') =}')
+with_vowels = 'This is so much fun!'
+vowels = 'aeiou'
+print(f'{"".join(char in for char in with_vowels if char not in vowels) =}')
