0de27b100d0fb4ef2747520ea054344460e39cf4
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-25 17:46:03 -0800

    Do not have to typecast to list

--- Python3/Bootcamp/Lists/nested_comprehension.py
+++ Python3/Bootcamp/Lists/nested_comprehension.py
@@ -2,4 +2,4 @@ nested_list = [list(range(1,4)), list(range(4,7)), list(range(8,10))]
 print(f'{nested_list =}')
 print(f'{[[single_list for single_list in nested_list] for single_list in nested_list] =}')
 
-print(f'{[["X" if num % 2 == 0 else "O" for num in list(range(1,4))] for num in list(range(1,4))] =}')
+print(f'{[["X" if num % 2 == 0 else "O" for num in range(1,4)] for num in range(1,4)] =}')
