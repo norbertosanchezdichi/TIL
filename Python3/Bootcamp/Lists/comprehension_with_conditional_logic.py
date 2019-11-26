67c6cf054a10740c9073823f9ff0fdf260d59c2e
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-25 16:41:07 -0800

    Print 'with_vowels'

--- Python3/Bootcamp/Lists/comprehension_with_conditional_logic.py
+++ Python3/Bootcamp/Lists/comprehension_with_conditional_logic.py
@@ -5,4 +5,5 @@ print(f'{[number for number in numbers if number % 2 != 0] =}')
 print(f'{[number * 2 if number % 2 == 0 else number / 2 for number in numbers] =}')
 
 with_vowels = 'This is so much fun!'
+print(f'{with_vowels =}')
 print(f'{"".join([char for char in with_vowels if char not in "aeiou"]) =}')
