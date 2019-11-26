34cbcb7424cdbaeab3ba57e677391ccbd93bc516
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-25 17:39:27 -0800

    Add another example

--- Python3/Bootcamp/Lists/nested_comprehension.py
+++ Python3/Bootcamp/Lists/nested_comprehension.py
@@ -1,3 +1,5 @@
 nested_list = [list(range(1,4)), list(range(4,7)), list(range(8,10))]
 print(f'{nested_list =}')
 print(f'{[[number for number in nested_list] for single_list in nested_list] =}')
+
+print(f'{[["X" if num % 2 == 0 else "O" for num in list(range(1,4))] for val in list(range(1,4))]}')
