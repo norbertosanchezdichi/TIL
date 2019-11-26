14ca2f4dd5ebb9349e4a0c6fdbfc94ba1e9ccd22
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-25 16:39:21 -0800

    Do not use 'vowels' string

--- Python3/Bootcamp/Lists/comprehension_with_conditional_logic.py
+++ Python3/Bootcamp/Lists/comprehension_with_conditional_logic.py
@@ -5,5 +5,4 @@ print(f'{[number for number in numbers if number % 2 != 0] =}')
 print(f'{[number * 2 if number % 2 == 0 else number / 2 for number in numbers] =}')
 
 with_vowels = 'This is so much fun!'
-vowels = 'aeiou'
-print(f'{"".join([char for char in with_vowels if char not in vowels]) =}')
+print(f'{"".join([char for char in with_vowels if char not in 'aeiou']) =}')
