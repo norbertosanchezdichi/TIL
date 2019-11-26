92cfb385a77a27a1fba174152e5a6b75c0996454
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-25 17:03:54 -0800

    Add another example

--- Python3/Bootcamp/Exercises/ListComprehension/list_comprehension.py
+++ Python3/Bootcamp/Exercises/ListComprehension/list_comprehension.py
@@ -1,2 +1,7 @@
 print(f'{[name[0] for name in ["Elie", "Tim", "Matt"]] =}')
 print(f'{[num for num in list(range(1,7)) if num % 2 == 0] =}')
+print(f'{[num for num in list(range(1,5)) if num in list(range(3,7))] =}')
+
+names = ["Elie", "Tim", "Matt"]
+print(f'{names =}')
+print(f'{[name[::-1].lower() for name in names] =}')
