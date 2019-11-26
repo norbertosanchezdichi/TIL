5213de53ee027b5766a46f7ab8530ca905b05f21
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-25 17:29:37 -0800

    Use 'join' method

--- Python3/Bootcamp/Lists/nested_comprehension.py
+++ Python3/Bootcamp/Lists/nested_comprehension.py
@@ -1,3 +1,3 @@
 nested_list = [list(range(1,4)), list(range(4,7)), list(range(8,10))]
 print(f'{nested_list =}')
-print(f'{[[single_list for single_list in nested_list] for number in nested_list]}')
+print(f'{[["".join(number) for number in nested_list] for single_list in nested_list] =}')
