9eb4420b124f6f83e03a0e3e02eba063e370eb9c
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-25 16:38:42 -0800

    Remove extra 'in' keyword

--- Python3/Bootcamp/Lists/comprehension_with_conditional_logic.py
+++ Python3/Bootcamp/Lists/comprehension_with_conditional_logic.py
@@ -6,4 +6,4 @@ print(f'{[number * 2 if number % 2 == 0 else number / 2 for number in numbers] =
 
 with_vowels = 'This is so much fun!'
 vowels = 'aeiou'
-print(f'{"".join([char in for char in with_vowels if char not in vowels]) =}')
+print(f'{"".join([char for char in with_vowels if char not in vowels]) =}')
