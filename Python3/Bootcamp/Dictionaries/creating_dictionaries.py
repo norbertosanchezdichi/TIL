b6ab6d9d7bbdc3f7f8b24b4a1d881d5d6dabfd81
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-26 20:36:31 -0800

    Initial commit

---
+++ Python3/Bootcamp/Dictionaries/creating_dictionaries.py
@@ -0,0 +1,12 @@
+student = {
+    'name': 'Norberto',
+    'owns_dog' : True,
+    'favorite_language': 'Python',
+    25 : 'my favorite number!'}
+
+print(f'{instructor =}')
+
+parrot = {'name': 'Lorenzo', 'age': 1.5, 'isLoco': True}
+
+print(f'{parrot =}')
+
