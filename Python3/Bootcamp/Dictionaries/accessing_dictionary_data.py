c10dfc21472cac7f30fd103875558ff3a712b185
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-26 20:54:52 -0800

    Initial commit

---
+++ Python3/Bootcamp/Dictionaries/accessing_dictionary_data.py
@@ -0,0 +1,9 @@
+student = {
+    'name': 'Norberto',
+    'owns_parrot' : True,
+    'favorite_language': 'Python',
+    25 : 'my favorite number!'}
+
+print(f'{student =}')
+
+print(f'{student["name"] =}')
