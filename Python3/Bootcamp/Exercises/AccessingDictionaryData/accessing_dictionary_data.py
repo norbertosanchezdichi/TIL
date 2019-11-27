36d50d6ac0c2cdf51b81a689721f1f90c31cc555
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-26 21:27:54 -0800

    Add missing parenthesis at the end

--- Python3/Bootcamp/Exercises/AccessingDictionaryData/accessing_dictionary_data.py
+++ Python3/Bootcamp/Exercises/AccessingDictionaryData/accessing_dictionary_data.py
@@ -9,7 +9,7 @@ print('Concatenation Solution')
 print(f'{artist["first"] + " " + artist["last"] =}')
 
 print('Format() Solution')
-print(f'{"{} {}".format(artist["first"], artist["last"] =}')
+print(f'{"{} {}".format(artist["first"], artist["last"]) =}')
 
 print('f-String Solution')
 print(f'{artist["first"]} {artist["last"] =}')
