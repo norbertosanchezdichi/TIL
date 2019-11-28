09beea2a2a2c7dd9444834569dfae8a2cef121a6
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-28 14:42:06 -0800

    Add second method of updating dictionary

--- Python3/Bootcamp/Exercises/DictionaryMethods/dictionary_methods.py
+++ Python3/Bootcamp/Exercises/DictionaryMethods/dictionary_methods.py
@@ -4,6 +4,8 @@ print(f'{inventory =}')
 stock_list = inventory.copy()
 print(f'{stock_list =}')
 
+stock_list['hot cheetos'] = 25
 stock_list.update({'cookie' : 18})
+
 stock_list.pop('cake')
 print(f'{stock_list =}')
