d1322f8beba1bc2510c9c2b630c655c64c1a1438
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-27 23:26:29 -0800

    Initial commit

---
+++ Python3/Bootcamp/Dictionaries/pop_popitems_update.py
@@ -0,0 +1,9 @@
+student = {
+    'name': 'Norberto',
+    'owns_parrot' : True,
+    'favorite_language': 'Python',
+    25 : 'my favorite number!'}
+print(f'{student =}')
+
+print(f'{student.pop(25) =}')
+print(f'{student =}')
