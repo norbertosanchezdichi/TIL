1a7d65c785851550f9efde1161f2572c8ad04ffa
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-27 22:07:49 -0800

    Initial commit

---
+++ Python3/Bootcamp/Dictionaries/using_in_with_dictionaries.py
@@ -0,0 +1,8 @@
+student = {
+    'name': 'Norberto',
+    'owns_parrot' : True,
+    'favorite_language': 'Python',
+    25 : 'my favorite number!'}
+    
+print(f"{'name' in student =}")
+print(f"{'Norberto' in student.values() =}")
