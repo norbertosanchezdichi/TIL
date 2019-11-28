26eb774443b4501dd16e0f5c1d29a5249246cc6a
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-28 14:34:28 -0800

    Initial commit

---
+++ Python3/Bootcamp/Exercises/DictionaryMethods/dictionary_methods.py
@@ -0,0 +1,9 @@
+inventory = {'croissant': 19, 'bagel': 4, 'muffin': 8, 'cake': 1}
+print(f'{inventory =}')
+
+stock_list = inventory.copy()
+print(f'{stock_list =}')
+
+stock_list.update({'cookie' : 18})
+stock_list.pop('cake')
+print(f'{stock_list =}')
