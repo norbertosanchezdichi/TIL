68b793e39007acdf085429491c8e4d79c9ddfe76
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-28 15:05:45 -0800

    Use double-quotes

--- Python3/Bootcamp/Dictionaries/comprehension.py
+++ Python3/Bootcamp/Dictionaries/comprehension.py
@@ -2,4 +2,4 @@ numbers = dict(first = 1, second = 2, third = 3)
 
 print(f'{numbers =}')
 
-print(f"{{{key:value ** 2 for key, value in numbers.items()} =}}")
+print(f""{{key:value ** 2 for key, value in numbers.items()} =}"")
