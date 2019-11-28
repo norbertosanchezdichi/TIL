b429d44725bca61faa410006d8b3d9319c4a0f0d
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-28 15:32:40 -0800

    Add '=' to print operation as well as result

--- Python3/Bootcamp/Dictionaries/comprehension.py
+++ Python3/Bootcamp/Dictionaries/comprehension.py
@@ -3,4 +3,4 @@ numbers = dict(first = 1, second = 2, third = 3)
 print(f'{numbers =}')
 print(f"{ {key:value ** 2 for key, value in numbers.items()} =}")
 
-print(f"{ {num: num ** 2 for num in list(range(1,6))} }")
+print(f"{ {num: num ** 2 for num in list(range(1,6))} =}")
