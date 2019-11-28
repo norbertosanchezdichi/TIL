76d5af8df13412587f29521a85e67baad1901e18
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-27 21:54:16 -0800

    Add for-loop to iterate using dictionary methods

--- Python3/Bootcamp/Dictionaries/iterating_over_dictionaries.py
+++ Python3/Bootcamp/Dictionaries/iterating_over_dictionaries.py
@@ -5,5 +5,14 @@ student = {
     25 : 'my favorite number!'}
 
 print(f'{student.values() =}')
+for value in student.values():
+    print(value)
+
 print(f'{student.keys() =}')
+for key in student.keys():
+    print(key)
+    
 print(f'{student.items() =}')
+for key, value in student.items():
+    print(key, value)
+
