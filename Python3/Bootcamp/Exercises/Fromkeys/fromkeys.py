7e8935ae236f1637b2ef7df20bfec12afb737e09
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-27 23:23:34 -0800

    Print game_properties and operation and result of fromkeys(..) method

--- Python3/Bootcamp/Exercises/Fromkeys/fromkeys.py
+++ Python3/Bootcamp/Exercises/Fromkeys/fromkeys.py
@@ -1,3 +1,5 @@
 game_properties = ["current_score", "high_score", "number_of_lives", "items_in_inventory", "power_ups", "ammo", "enemies_on_screen", "enemy_kills", "enemy_kill_streaks", "minutes_played", "notifications", "achievements"] 
+print(f'{game_properties =}')
 
-initial_game_state = dict.fromkeys(game_properties, 0)
+
+print(f'{dict.fromkeys(game_properties, 0) =}')
