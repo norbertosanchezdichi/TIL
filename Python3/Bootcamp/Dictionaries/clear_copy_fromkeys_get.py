37049dffdafa7a0944c869e78496a65379f0ce39
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-27 22:26:21 -0800

    Initial commit

---
+++ Python3/Bootcamp/Dictionaries/clear_copy_fromkeys_get.py
@@ -0,0 +1,16 @@
+student = {
+    'name': 'Norberto',
+    'owns_parrot' : True,
+    'favorite_language': 'Python',
+    25 : 'my favorite number!'}
+
+print(f'{student = }')
+
+student_copy = student.copy()
+print(f'{student_copy = }')
+
+student_copy.clear()
+print(f'{studnet_copy = }')
+
+new_student = {}.fromkeys(student.keys(), 'None'
+print(f'{new_student = }'))
