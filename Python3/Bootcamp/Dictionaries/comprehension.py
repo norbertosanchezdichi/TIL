b1b9b92b5d8d6bf19c41457c5899af50fc7c005a
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-28 15:31:38 -0800

    Add another example

--- Python3/Bootcamp/Dictionaries/comprehension.py
+++ Python3/Bootcamp/Dictionaries/comprehension.py
@@ -1,5 +1,6 @@
 numbers = dict(first = 1, second = 2, third = 3)
 
 print(f'{numbers =}')
-
 print(f"{ {key:value ** 2 for key, value in numbers.items()} =}")
+
+print(f"{ {num: num ** 2 for num in list(range(1,6))} }")
