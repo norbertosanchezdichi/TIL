8aa640598c3c41e85560cfbbcae406220063fb3c
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-27 21:36:48 -0800

    Initial commit

---
+++ Python3/Bootcamp/Dictionaries/iterating_over_dictionaries.py
@@ -0,0 +1,9 @@
+student = {
+    'name': 'Norberto',
+    'owns_parrot' : True,
+    'favorite_language': 'Python',
+    25 : 'my favorite number!'}
+
+print(f'{student.values() =}')
+
+print(f'{student.keys() =}')
