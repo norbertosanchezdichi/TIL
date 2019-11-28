d688fd7b4cc5215bf1fd7a1cf08d6f0865ee5ae8
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-27 22:04:01 -0800

    Add two more solutions using sum(..)

--- Python3/Bootcamp/Exercises/TotalingDonations/totaling_donations.py
+++ Python3/Bootcamp/Exercises/TotalingDonations/totaling_donations.py
@@ -4,7 +4,14 @@ print(f'{donations.items() =}')
 
 total_donations = 0.0
 
+print('Using for-loop')
 for donation_value in donations.values():
     total_donations += donation_value
     
 print(f'{total_donations =}')
+
+print('Using sum(..) with for-loop')
+print(f'{sum(donation for donation in donations.values()) =}')
+
+print('Using sum(..) without for-loop')
+print(f'{sum(donations.values()) =}')
