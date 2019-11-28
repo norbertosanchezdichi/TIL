c19367264d0f8f9590dfed2008ca356802771e5f
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-28 14:55:23 -0800

    Initial commit

---
+++ Python3/Bootcamp/Exercises/SpotifyPlaylist/spotify_playlist.py
@@ -0,0 +1,18 @@
+playlist = {'title' : 'patagonia bus', 
+            'author' : 'Norberto Sánchez-Dichi',
+            'songs' : [
+                {'title': 'song1',
+                 'artist': ['blue'],
+                 'duration' : 2.5
+                },
+                {'title': 'song2',
+                 'artist': ['kitty', 'djcat'],
+                 'duration' : 5.25
+                },
+                {'title': 'woofwoof',
+                 'artist': ['Courage'],
+                 'duration' : 2.0
+                }
+            ]
+}
+print(f'{playlist =}')               
