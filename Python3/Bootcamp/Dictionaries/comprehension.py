c8e8c5e10a5f1351df5fd9a0d5526b62a2cef67e
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-30 14:38:04 -0800

    Print string1 and string2

--- Python3/Bootcamp/Dictionaries/comprehension.py
+++ Python3/Bootcamp/Dictionaries/comprehension.py
@@ -6,5 +6,7 @@ print(f"{ {key:value ** 2 for key, value in numbers.items()} =}")
 print(f"{ {num: num ** 2 for num in list(range(1,6))} =}")
 
 string1 = 'ABC'
+print(f"{string1 =}")
 string2 = '123'
+print(f"{string2 =}")
 print(f"{ {string1[i]: string2[i] for i in range(0, len(string1))} =}")
