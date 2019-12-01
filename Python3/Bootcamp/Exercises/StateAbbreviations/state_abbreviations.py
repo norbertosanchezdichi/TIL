51ea3957418ddb0d445152b45554837555eafc49
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-30 20:58:18 -0800

    Also use zip(..) method

--- Python3/Bootcamp/Exercises/StateAbbreviations/state_abbreviations.py
+++ Python3/Bootcamp/Exercises/StateAbbreviations/state_abbreviations.py
@@ -2,3 +2,5 @@ list1 = ["CA", "NJ", "RI"]
 list2 = ["California", "New Jersey", "Rhode Island"]
 
 print(f"{ {list1[i]: list2[i] for i in range(0,len(list1))} =}")
+
+print(f"{ dict(zip(list1, list2)) =}")
