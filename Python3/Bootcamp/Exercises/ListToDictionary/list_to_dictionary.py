24663c8006841fbf56e8e70d57b61b2c7111dbae
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-11-30 21:15:51 -0800

    Add two more simple methods

--- Python3/Bootcamp/Exercises/ListToDictionary/list_to_dictionary.py
+++ Python3/Bootcamp/Exercises/ListToDictionary/list_to_dictionary.py
@@ -1,4 +1,11 @@
 person = [["name", "Jared"], ["job", "Musician"], ["city", "Bern"]]
 print(f'{person =}')
 
+print('Using Dictionary Comprehension')
 print(f"{ {person[i][0]: person[i][1] for i in range(0, len(person))} =}")
+
+print('Using Dictionary Comprehension Without References to List Indexes')
+print(f"{ {keys:values for keys,values in person} =}")
+
+print('Using dict(..)')
+print(f"{dict(person) =}")
