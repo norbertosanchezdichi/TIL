52f29d28ff9373e149172011e2cdec5e6e2b3ff1
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2019-12-01 21:24:02 -0800

    Add example of tuple-keys in dictionary

--- Python3/Bootcamp/TuplesAndSets/accessing_data.py
+++ Python3/Bootcamp/TuplesAndSets/accessing_data.py
@@ -3,3 +3,8 @@ print(f'{first_tuple =}')
 
 print(f'{first_tuple[1] =}')
 print(f'{first_tuple[-1] =}')
+
+locations = {
+    {(35.6895, 39.6917): "Tokyo Office"
+}
+print(f'{locations[(35.6895, 39.6917)] =}')
