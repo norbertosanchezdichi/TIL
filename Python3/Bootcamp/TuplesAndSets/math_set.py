64fcbb87f09951895d849f7621d0cf8dbe9ff397
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-01-12 19:34:16 -0800

    Initial commit

---
+++ Python3/Bootcamp/TuplesAndSets/math_set.py
@@ -0,0 +1,8 @@
+socal_cities = {'Los Angeles', 'San Diego', 'Montebello', 'Whittier'}
+norcal_cities = {'San Francisco', 'San Jose', 'Sunnyvale', 'Santa Clara'}
+
+print(f'{socal_cities =}')
+print(f'{norcal_cities =}')
+
+print(f'{socal_cities | norcal_cities =}')
+print(f'{socal_cities & norcal_cities =}')
