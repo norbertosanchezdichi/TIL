511c6dc6fc39442cf10097dfa65f828656f7bb71
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-02-25 21:59:31 -0800

    Use double quotation-marks for string of vowels inside f-string

--- Python3/Bootcamp/TuplesAndSets/comprehension_set.py
+++ Python3/Bootcamp/TuplesAndSets/comprehension_set.py
@@ -2,4 +2,4 @@ print(f'{ {x**2 for x in range(10)} =}')
 
 string = 'Norberto'
 print(f'{string =}')
-print(f'{ {char for char in string if char in 'aeiou'} =}')
+print(f'{ {char for char in string if char in "aeiou"} =}')
