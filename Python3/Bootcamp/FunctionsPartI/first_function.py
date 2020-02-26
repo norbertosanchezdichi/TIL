6fa2f7d80fb3fcb360f4220092c14bca1dc8be33
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-02-25 22:21:07 -0800

    Initial commit

---
+++ Python3/Bootcamp/FunctionsPartI/first_function.py
@@ -0,0 +1,7 @@
+def sing_happy_birthday():
+    print('Happy Birthday To You')
+    print('Happy Birthday To You')
+    print('Happy Birthday Dear You')
+    print('Happy Birthday To You')
+
+sing_happy_birthday()
