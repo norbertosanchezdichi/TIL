8c84aaf3becdaf301cdba750ab83699f572f5c3a
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-02-26 21:05:34 -0800

    Fix syntax error by adding ':' after definition of function

--- Python3/Bootcamp/FunctionsPartI/print_square_of_7.py
+++ Python3/Bootcamp/FunctionsPartI/print_square_of_7.py
@@ -1,9 +1,9 @@
-def print_square_of_7()
+def print_square_of_7():
     print(7**2)
     
 print_square_of_7()
 
-def square_of_7()
+def square_of_7():
     return 7**2
     print('I AM AFTER THE RETURN!')
     
