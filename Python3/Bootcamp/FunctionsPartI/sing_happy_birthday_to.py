7ddc99f822a471ec2897732cb098bf5252ae93e5
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-02-26 21:32:56 -0800

    Initial commit

---
+++ Python3/Bootcamp/FunctionsPartI/sing_happy_birthday_to.py
@@ -0,0 +1,9 @@
+def sing_happy_birthday_to(name):
+
+    print("Happy Birthday To You")
+    print("Happy Birthday To You")
+    print(f"Happy Birthday Dear {name}")
+    print("Happy Birthday To You")
+    
+sing_happy_birthday_to("Bojack")
+sing_happy_birthday_to("Diane")
