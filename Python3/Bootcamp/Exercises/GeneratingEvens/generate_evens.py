0458709d7a18dbc2d6c1f3fac28fc792eb6fb3a2
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-02-26 21:22:42 -0800

    Define what evenNumber is

--- Python3/Bootcamp/Exercises/GeneratingEvens/generate_evens.py
+++ Python3/Bootcamp/Exercises/GeneratingEvens/generate_evens.py
@@ -1,4 +1,4 @@
 def generate_evens():
-    return [evenNumber if (evenNumber % 2 == 0) in range(0, 50)]
+    return [evenNumber if (evenNumber % 2 == 0) for evenNumber in range(0, 50)]
     
 print(generate_evens())
