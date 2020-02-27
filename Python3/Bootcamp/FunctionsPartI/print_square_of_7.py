65d2916c3d2de95c0a9eb08939798dabfdaaced3
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-02-26 21:04:37 -0800

    Initial commit

---
+++ Python3/Bootcamp/FunctionsPartI/print_square_of_7.py
@@ -0,0 +1,11 @@
+def print_square_of_7()
+    print(7**2)
+    
+print_square_of_7()
+
+def square_of_7()
+    return 7**2
+    print('I AM AFTER THE RETURN!')
+    
+result = square_of_7()
+print(result)
