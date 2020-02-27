73cf46e94bde80e318500c8a147dabdddacd1a01
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-02-26 21:27:47 -0800

    Do not include 50

--- Python3/Bootcamp/Exercises/GeneratingEvens/generate_evens.py
+++ Python3/Bootcamp/Exercises/GeneratingEvens/generate_evens.py
@@ -1,4 +1,4 @@
 def generate_evens():
-    return [evenNumber for evenNumber in range(1, 51) if (evenNumber % 2) == 0]
+    return [evenNumber for evenNumber in range(1, 50) if (evenNumber % 2) == 0]
     
 print(generate_evens())
