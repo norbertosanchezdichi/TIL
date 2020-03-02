5f8e26d33d29dc7ba2520bf2511804fa2a278a52
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-01 19:37:51 -0800

    No parenthesis required when nesting method calls

--- Python3/Bootcamp/Exercises/SingleLetterCount/single_letter_count.py
+++ Python3/Bootcamp/Exercises/SingleLetterCount/single_letter_count.py
@@ -1,5 +1,4 @@
 def single_letter_count(string, letter):
-    return (string.lower()).count(letter.lower())
+    return string.lower().count(letter.lower())
     
 print (single_letter_count('Norberto', 'O'))
-    
