8cf99658735c1c54760df95039364ec7677ee12f
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-01 18:53:20 -0800

    Initial commit

---
+++ Python3/Bootcamp/Exercises/ReturnDay/return_day.py
@@ -0,0 +1,4 @@
+def return_day(number):
+    day_of_the_week = {1:"Sunday", 2:"Monday", 3:"Tuesday", 4:"Wednesday", 5:"Thursday", 6:"Friday", 7:"Saturday"}
+    
+    return day_of_the_week.get(number, None)
