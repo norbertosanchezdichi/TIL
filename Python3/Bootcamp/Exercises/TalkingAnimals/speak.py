7a2d50199a653c91ff7e6994c7aef1ad06d85269
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-01 17:45:32 -0800

    Initial commit

---
+++ Python3/Bootcamp/Exercises/TalkingAnimals/speak.py
@@ -0,0 +1,14 @@
+def speak(animal = "dog"):
+
+    if animal == "pig":
+        return "oink"
+    elif animal == "duck":
+        return "quack"
+    elif animal == "cat":
+        return "meow:
+    elif animal == "dog":
+        return "woof"
+    else:
+        return "?"
+        
+speak()
