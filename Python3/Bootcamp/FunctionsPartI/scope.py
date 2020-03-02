e8556940e2c0e7fe65030297e0d66309aacd3208
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-01 18:13:05 -0800

    Initial commit

---
+++ Python3/Bootcamp/FunctionsPartI/scope.py
@@ -0,0 +1,18 @@
+ instructor1 = "Mary"
+ def say_hello():
+    instructor2 = "Norberto"
+    return f'Hello {instructor1}'
+    
+print(say_hello())
+print(instructor2) #NameError
+
+total = 0
+def increase_total():
+    total += 1  #UnboundLocalError: local variable "total" not found.  Must use "global" keyword.
+    return total
+    
+total = 0
+def increase_total():
+    global total
+    total += 1
+    return total
