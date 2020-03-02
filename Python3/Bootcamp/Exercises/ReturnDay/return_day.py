25d0eea1086ffa95011a46049e417d20a346b014
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-01 18:54:22 -0800

    Print output of function

--- Python3/Bootcamp/Exercises/ReturnDay/return_day.py
+++ Python3/Bootcamp/Exercises/ReturnDay/return_day.py
@@ -2,3 +2,6 @@ def return_day(number):
     day_of_the_week = {1:"Sunday", 2:"Monday", 3:"Tuesday", 4:"Wednesday", 5:"Thursday", 6:"Friday", 7:"Saturday"}
     
     return day_of_the_week.get(number, None)
+    
+print(return_day(6))
+print(return_day())
