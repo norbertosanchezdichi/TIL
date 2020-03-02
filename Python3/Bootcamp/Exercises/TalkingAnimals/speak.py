c7d6d089ed9c7d66479def29180556f219f11d54
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-01 17:58:01 -0800

    Add ultra-compact solution

--- Python3/Bootcamp/Exercises/TalkingAnimals/speak.py
+++ Python3/Bootcamp/Exercises/TalkingAnimals/speak.py
@@ -26,3 +26,12 @@ def speak(animal = "dog"):
     return "?"
     
 print(speak())
+
+# Ultra-Compact solution
+
+def speak(animal='dog'):
+    noises = {'pig':'oink', 'duck':'quack', 'cat':'meow', 'dog':'woof'}
+    
+    return noises.get(animal, '?')
+
+print(speak())
