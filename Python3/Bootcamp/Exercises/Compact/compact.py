c8285e461aed5334ed6dd9eeb744123bd6ba550a
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-15 16:10:15 -0700

    Use or logic instead of and

--- Python3/Bootcamp/Exercises/Compact/compact.py
+++ Python3/Bootcamp/Exercises/Compact/compact.py
@@ -1,4 +1,4 @@
 def compact(list):
-    return [value for value in list if value != False and value != None]
+    return [value for value in list if value != False or value != None]
     
 print(compact([0,1,2,"",[], False, {}, None, "All done"]))
