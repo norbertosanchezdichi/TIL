dd088fdf372274c7e07845e761565f594b0e5ff6
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-15 16:22:43 -0700

    Add most compact version

--- Python3/Bootcamp/Exercises/Compact/compact.py
+++ Python3/Bootcamp/Exercises/Compact/compact.py
@@ -1,4 +1,7 @@
 def compact(list):
     return [value for value in list if value != False and value != None and len(str(value)) != 0 and not not value]
     
+def compact(list):
+    return [value for value in list if value]    
+    
 print(compact([0,1,2,"",[], False, {}, None, "All done"]))
