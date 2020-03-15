e50fb34e7cc090245d8e20e11450fefbd0dfe65c
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-15 16:09:24 -0700

    Check for None values

--- Python3/Bootcamp/Exercises/Compact/compact.py
+++ Python3/Bootcamp/Exercises/Compact/compact.py
@@ -1,4 +1,4 @@
 def compact(list):
-    return [value for value in list if value != False]
+    return [value for value in list if value != False and value != None]
     
 print(compact([0,1,2,"",[], False, {}, None, "All done"]))
