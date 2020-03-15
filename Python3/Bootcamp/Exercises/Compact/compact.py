da08d25e19cd4442ee28b4241fca58f207eb2d8e
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-15 16:11:49 -0700

    Check for length

--- Python3/Bootcamp/Exercises/Compact/compact.py
+++ Python3/Bootcamp/Exercises/Compact/compact.py
@@ -1,4 +1,4 @@
 def compact(list):
-    return [value for value in list if value != False or value != None]
+    return [value for value in list if value != False and value != None and len(value) != 0]
     
 print(compact([0,1,2,"",[], False, {}, None, "All done"]))
