e6bcac4c66ba60f23774dd8d72ee7aa3168fd46f
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-15 22:57:44 -0700

    Initial commit

---
+++ Python3/Bootcamp/FunctionsPartII/contains_purple.py
@@ -0,0 +1,7 @@
+def contains_purple(*args):
+    if 'purple' in args:
+        return True
+    return False
+    
+print(contains_purple(25, 'purple'))
+print(contains_purple('green', False, 37, 'blue', 'hello world'))
---
+++ Python3/Bootcamp/FunctionsPartII/fav_colors.py
@@ -0,0 +1,5 @@
+def fav_colors(**kwargs):
+    for person, color in kwargs.items():
+    print(f"{person}'s favorite color is {color}")
+
+fav_colors(norberto="blue", lorenzo="green")
