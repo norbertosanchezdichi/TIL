5a3ef25b10f04f4c9bcfffa3f995c81940ba7245
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-16 00:30:54 -0700

    Add new example

--- Python3/Bootcamp/FunctionsPartII/dictionary_unpacking.py
+++ Python3/Bootcamp/FunctionsPartII/dictionary_unpacking.py
@@ -3,3 +3,11 @@ def display_names(first, second):
     
 names = {"first": "Norberto", "second": "Lorenzo"}
 display_names(**names)
+
+def add_and_multiply_numbers(a, b, c, **kwargs):
+    print(a + b * c)
+    print(kwargs)
+    
+data = dict(a=1, b=2, c=3, d=55, name="the Tiger")
+
+add_and_multiply_numbers(**data, cat="blue")
