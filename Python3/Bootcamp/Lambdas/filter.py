a64223676f70d8e30254da718163061db7aaedf1
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-18 22:13:12 -0700

    Typecast filter object to list

--- Python3/Bootcamp/Lambdas/filter.py
+++ Python3/Bootcamp/Lambdas/filter.py
@@ -5,7 +5,7 @@ print(listOfNums)
 print(evens)
 
 names = ['austin', 'penny', 'anthony', 'angel', 'billy']
-a_names = filter(lambda n: n[0]=='a', names)
+a_names = list(filter(lambda n: n[0]=='a', names))
 print(names)
 print(a_names)
 
