fc1bc33f3473b14ae71da69e8bff18d6329f1961
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-18 22:09:42 -0700

    Initial commit

---
+++ Python3/Bootcamp/Lambdas/filter.py
@@ -0,0 +1,15 @@
+listOfNums = [1,2,3,4]
+
+evens = list(filter(lambda x: x % 2 == 0, listOfNums))
+print(listOfNums)
+print(evens)
+
+names = ['austin', 'penny', 'anthony', 'angel', 'billy']
+a_names = filter(lambda n: n[0]=='a', names)
+print(names)
+print(a_names)
+
+names = ['Lassie', 'Norberto', 'Rusty']
+
+print(names)
+print(f'{list(map(lambda name: f"Your instructor is {name}", filter(lambda v: len(v) > 6), names)) =}')
