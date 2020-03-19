ffe3f762ba5b45115c8a8a9a74fbb2fed85d0c48
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-18 21:34:11 -0700

    Remove all assignment operators inside curly braces

--- Python3/Bootcamp/Lambdas/map.py
+++ Python3/Bootcamp/Lambdas/map.py
@@ -2,11 +2,11 @@ nums = [1, 2, 3, 4]
 doubles = map(lambda x: x*2,nums)
 
 print(f'{doubles.__name__}')
-print(f'{list(doubles) =}')
-print(f'{list(doubles) =}')
+print(f'{list(doubles)}')
+print(f'{list(doubles)}')
 
 doubles = list(map(lambda x: x*2))
-print(f'{doubles =}')
+print(f'{doubles}')
 
 evens = list(map(lambda x: x*2, nums))
-print(f'{evens =}')
+print(f'{evens}')
