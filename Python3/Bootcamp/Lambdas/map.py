930e85f213c89fa49ff6fb0e8555d60c0e6bc31b
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-18 21:43:23 -0700

    Remove redundant doubles function definition

--- Python3/Bootcamp/Lambdas/map.py
+++ Python3/Bootcamp/Lambdas/map.py
@@ -1,11 +1,8 @@
-nums = [1, 2, 3, 4]
+nums = [1, 2, 3, 4]dds
 doubles = map(lambda x: x*2,nums)
 
 print(f'{list(doubles)}')
 print(f'{list(doubles)}')
-
-doubles = list(map(lambda x: x*2))
-print(f'{doubles}')
 
 evens = list(map(lambda x: x*2, nums))
 print(f'{evens}')
