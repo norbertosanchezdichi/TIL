26b0ded8c1f53f2034dee318741c275bbce6b5e4
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-20 22:20:15 -0700

    Initial commit

---
+++ Python3/Bootcamp/Lambdas & Built-In Functions/reversed.py
@@ -0,0 +1,11 @@
+nuns = [1, 2, 3, 4]
+print(reversed(nuns))
+
+for char in reversed("hellow world"):
+    print (char)
+    
+print("hello"[::-1])
+print(''.join(list(reversed("hello"))))
+
+for x in reversed(range(0, 10)):
+    print(x)
