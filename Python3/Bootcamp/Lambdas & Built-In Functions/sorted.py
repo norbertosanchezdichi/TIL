5266d7a508dd892ad8933926a393222f5a14c5ea
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-20 21:41:21 -0700

    Add examples sorting a dictionary

--- Python3/Bootcamp/Lambdas & Built-In Functions/sorted.py
+++ Python3/Bootcamp/Lambdas & Built-In Functions/sorted.py
@@ -1,4 +1,21 @@
+#List of Numbers
 more_numbers = [6, 1, 8, 2]
 print(f'more_numbers = {more_numbers}')
 print(f'sorted more_numbers = {sorted(more_numbers)}')
 print(f'sorted more_numbers in reverse = {sorted(more_numbers, reverse = True)}')
+
+#Dictionary
+
+users = [
+    {"username": "samuel", "tweets": ["I love cake", "i love fruit"]},
+    {"username": "katie", "tweets": ["I love my cate"]},
+    {"username": "jeff", "tweets": [], "color": "purple"},
+    {"username": "bob123", "tweets": [], "num": 10, "color": "teal"},
+    {"username": "doggo_luvr", "tweets": ["dogs are the best"]},
+    {"username": "guitar_gal", "tweets": []},
+]
+
+print(f'users = {users}')
+print(f'sorted users by length = {sorted(users, key=len)}
+print(f'sorted users by alphabetical order = {sorted(users, key=lambda user: user['username'])}
+print(f'sorted users by alphabetical order = {sorted(users, key=lambda user: len(user['tweets']), reverse = True)}
