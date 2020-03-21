0c3e2e8278a67f653389a851195e73c5544f1a40
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-20 23:30:16 -0700

    Initial commit

--- Python3/Bootcamp/Lambdas & Built-In Functions/zip.py
+++ Python3/Bootcamp/Lambdas & Built-In Functions/zip.py
@@ -1,3 +1,4 @@
+#Part I
 nums1 = [1, 2, 3, 4, 5]
 nums2 = [6, 7, 8, 9, 10]
 
@@ -10,3 +11,24 @@ print(list(zip(words, nums1, nums2)))
 
 five_by_two = [(0, 1), (1, 2), (2, 3), (3, 4), (4, 5)]
 print(list(zip(*five_by_two)))
+
+#Part II
+midterms = [80, 91, 78]
+finals = [98, 89, 53]
+students = ['dan', 'ang', 'kate']
+
+#Using list
+final_grades = [pair[0]:max(pair[1], pair[2]) for pair in zip(students, midterms, finals)]
+print(final_grades)
+
+#Using zip(..) and map(..)
+final_grades = dict(
+    zip(
+        students,  
+        map(
+            lambda pair: max(pair),
+            zip(midterms, finals)
+            )
+        )
+)
+print(final_grades)
