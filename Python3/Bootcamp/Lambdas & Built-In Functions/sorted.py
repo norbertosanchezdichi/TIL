11df3b14fbcf868eaa1cea2b5b833fe2cf6784ce
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-20 21:43:03 -0700

    Fix syntaxError

--- Python3/Bootcamp/Lambdas & Built-In Functions/sorted.py
+++ Python3/Bootcamp/Lambdas & Built-In Functions/sorted.py
@@ -16,6 +16,6 @@ users = [
 ]
 
 print(f'users = {users}')
-print(f'sorted users by length = {sorted(users, key=len)}
-print(f'sorted users by alphabetical order = {sorted(users, key=lambda user: user['username'])}
-print(f'sorted users by alphabetical order = {sorted(users, key=lambda user: len(user['tweets']), reverse = True)}
+print(f'sorted users by length = {sorted(users, key=len)}')
+print(f'sorted users by alphabetical order = {sorted(users, key=lambda user: user['username'])}')
+print(f'sorted users by alphabetical order = {sorted(users, key=lambda user: len(user['tweets']), reverse = True)}')
