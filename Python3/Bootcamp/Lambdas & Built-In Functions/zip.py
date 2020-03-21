302101407f3e534526d5cdda4c3d6ef090ef2f9b
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-20 23:11:53 -0700

    Initial commit

---
+++ Python3/Bootcamp/Lambdas & Built-In Functions/zip.py
@@ -0,0 +1,12 @@
+nums1 = [1, 2, 3, 4, 5]
+nums2 = [6, 7, 8, 9, 10]
+
+print(list(zip(nums1, nums2)))
+print(dict(zip(nums1, nums2)))
+
+words = ['hi', 'ho', 'lol']
+
+print(list(zip(words, nums1, nums2))
+
+five_by_two = [(0, 1), (1, 2), (2, 3), (3, 4), (4, 5)]
+print(list(zip(*five_by_two)))
