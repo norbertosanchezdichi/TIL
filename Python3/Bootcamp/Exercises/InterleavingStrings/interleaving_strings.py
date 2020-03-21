8f83ea40b8d2f842d84976a2d0f73123d05f91d3
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-21 00:04:06 -0700

    Initial commit

--- Python3/Bootcamp/Exercises/InterleavingStrings/interleaving_strings.py
+++ Python3/Bootcamp/Exercises/InterleavingStrings/interleaving_strings.py
@@ -1,6 +1,13 @@
 def interleave(string1, string2):
     string1_expand = [char for char in string1]
     string2_expand = [char for char in string2]
-    return zip(string1_expand, string2_expand)
+    string1string2 = list(zip(string1_expand, string2_expand))
+    string_a = [c[0] + c[1] for c in string1string2]
+    
+    string = ''
+    for string_b in string_a:
+        string += string_b
+        
+    return string
     
 print(interleave('hi', 'ha'))
