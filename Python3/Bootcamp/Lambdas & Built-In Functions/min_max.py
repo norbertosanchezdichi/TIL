6c026607b7b339e42c0a716727fa6752bbef8416
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-20 22:06:38 -0700

    Replace quotes to avoid syntaxError

--- Python3/Bootcamp/Lambdas & Built-In Functions/min_max.py
+++ Python3/Bootcamp/Lambdas & Built-In Functions/min_max.py
@@ -1,10 +1,10 @@
 print(f'{max(3, 67 ,9)}')
 print(f'{max("awesome")}')
-print(f'{max({1:'a', 3:'c', 2:'b'})}')
+print(f"{max({1:'a', 3:'c', 2:'b'})}")
 
-print(f'{min(3, 67 ,9)}')
+print(f"{min(3, 67 ,9)}")
 print(f'{min("awesome")}')
-print(f'{min({1:'a', 3:'c', 2:'b'})}')
+print(f"{min({1:'a', 3:'c', 2:'b'})}")
 
 names = ['Arya', 'Samson', 'Dora', 'Tim', 'Ollivander']
 print(f'{min(len(name) for name in names)}')
@@ -17,5 +17,5 @@ songs = [
     {"title": "Toxic", "playcount": 31}
 ]
 
-print(f'{min(songs, key=lambda s: s['playcount'])}')
-print(f'{max(songs, key=lambda s: s['playcount'])['title']}')
+print(f"{min(songs, key=lambda s: s['playcount'])}")
+print(f"{max(songs, key=lambda s: s['playcount'])['title']}")
