06c824d391be7c085cd8e923a7dc1766429a53c7
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-20 21:30:35 -0700

    Initial commit

---
+++ Python3/Bootcamp/Lambdas & Built-In Functions/sorted.py
@@ -0,0 +1,4 @@
+more_numbers = [6, 1, 8, 2]
+print(f'more_numbers = {more_numbers}')
+print(f'sorted more_numbers = {sorted(more_numbers)}')
+print(f'sorted more_numbers in reverse = {sorted(more_numbers, reverse = True)}')
