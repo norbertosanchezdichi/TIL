4211019f6d6178ac21ad5807d7806af55e408b3e
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-22 22:33:16 -0700

    Add more examples of exceptions

--- Python3/Bootcamp/DebuggingAndErrorHandling/colorize.py
+++ Python3/Bootcamp/DebuggingAndErrorHandling/colorize.py
@@ -1,7 +1,11 @@
 def colorize(text, color):
+    colors = ("cyan", "yellow", "blue", "green", "magenta")
     if type(text) is not str:
         raise TypeError("text must be instance of str")
-    
+    if type(color) is not str:
+        raise TypeError("color must be instance of str")
+    if colornot in colors:
+        raise ValueError("color is invalid color")
     print(f"Printed {text} in {color}")
     
 colorize("hello", "red")
