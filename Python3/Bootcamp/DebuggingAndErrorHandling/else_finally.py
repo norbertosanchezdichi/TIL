1d45eb87311236161f7bc8abd9aaf6757aad36f8
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-22 22:48:41 -0700

    Initial commit

---
+++ Python3/Bootcamp/DebuggingAndErrorHandling/else_finally.py
@@ -0,0 +1,11 @@
+while True:
+    try:
+        num = int(input("please enter a number: "))
+    except ValueError:
+        print("That is not a number!")
+    else:
+        print("Good job, you entered a number!")
+        break
+    finally:
+        print("RUNS NO MATTER WHAT!")
+print("REST OF GAME LOGIC RUNS!")
