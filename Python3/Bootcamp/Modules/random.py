9f578b6bfa5f09c1863424492fa058bb1242e508
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-22 23:26:47 -0700

    Initial commit

---
+++ Python3/Bootcamp/Modules/random.py
@@ -0,0 +1,11 @@
+import random
+
+print(random.choice(['apple', 'banana', 'cherry', 'durian']))
+print(random.randint(1,100))
+print(random.shuffle(['apple', 'banana', 'cherry', 'durian']))
+
+import random as omg_so_random
+
+print(omg_so_random.choice(['apple', 'banana', 'cherry', 'durian']))
+print(omg_so_random.randint(1,100))
+print(omg_so_random.shuffle(['apple', 'banana', 'cherry', 'durian']))
