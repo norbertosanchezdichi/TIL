b7794a6935160c45667742be070c93dc311371d8
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-22 22:05:55 -0700

    Add method using map(..)

--- Python3/Bootcamp/Exercises/ExtractFullName/extract_full_name.py
+++ Python3/Bootcamp/Exercises/ExtractFullName/extract_full_name.py
@@ -1,4 +1,9 @@
+#Using list comprehension
 def extract_full_name(listOfNames):
     return [name['first'] + ' ' + name['last'] for name in listOfNames] 
     
+#Using map(..)
+def extract_full_name(l):
+    return list(map(lambda val: "{} {}".format(val['first'], val['last']), l))
+
 print(extract_full_name([{'first': 'Norberto', 'last': 'Sánchez-Dichi'}, {'first': 'Ace', 'last': 'Ventura'}]))
