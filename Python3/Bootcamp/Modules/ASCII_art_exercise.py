4f11f90cb913f0f4198d1449175d1d523dee268a
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-23 00:14:48 -0700

    Add default color

--- Python3/Bootcamp/Modules/ASCII_art_exercise.py
+++ Python3/Bootcamp/Modules/ASCII_art_exercise.py
@@ -1,8 +1,13 @@
 import pyfiglet
 from termcolor import colored
 
+valid_colors = (red, green, yellow, blue, magenta, cyan, white)
+
 msg = input("What would you like to print? ")
 color = input("What color? ")
+
+if color not in valid_colors:
+    color = "blue"
 
 ascii_art = pyfiglet.figlet_format(msg)
 colored_ascii = colored(ascii_art, color = color)
