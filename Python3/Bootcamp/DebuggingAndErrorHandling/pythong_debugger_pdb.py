6aea76b85361bdb49c685a9fc0c524e42e54e19b
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-22 22:58:26 -0700

    Add another example

--- Python3/Bootcamp/DebuggingAndErrorHandling/pythong_debugger_pdb.py
+++ Python3/Bootcamp/DebuggingAndErrorHandling/pythong_debugger_pdb.py
@@ -3,8 +3,21 @@ import pdb
 first = "First"
 second = "Second"
 result = first + second
-pdb.set_trace()
+#pdb.set_trace()
 third = "Third"
 result += third
 print(result)
 
+def add_numbers(a, b, c, d):
+    import pdb; pdb.set_trace()
+    
+    return a + b + c + d
+
+# Also used in one line
+# import pdb; pdb.set_trace()
+
+# Common PDB Commands:
+# l (list)
+# n (next line)
+# c (exist)
+# p (print)
