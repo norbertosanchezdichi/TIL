02bc04391e786ea9ad29c53cc648c9e59a5155b4
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-22 22:49:34 -0700

    Add missing parenthesis

--- Python3/Bootcamp/DebuggingAndErrorHandling/divide.py
+++ Python3/Bootcamp/DebuggingAndErrorHandling/divide.py
@@ -2,7 +2,7 @@ def divide(a, b):
     try:
         result = a / b
     except (ZeroDivisionError, TypeError) as err:
-        print("Something went wrong!"
+        print("Something went wrong!")
         print(err)
     else:
         print(f"{a} divided by {b} is {result}")
