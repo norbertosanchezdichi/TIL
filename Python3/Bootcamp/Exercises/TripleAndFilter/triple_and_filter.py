13a0fdeb15cb8fda911a73cf48e986b2d7eeb930
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-22 21:35:01 -0700

    Add method using map(..)

--- Python3/Bootcamp/Exercises/TripleAndFilter/triple_and_filter.py
+++ Python3/Bootcamp/Exercises/TripleAndFilter/triple_and_filter.py
@@ -1,4 +1,9 @@
+#Using list comprehension
 def triple_and_filter(listOfNums):
     return [num * 3 for num in list(filter(lambda x: not x % 4, listOfNums))]
+    
+#Using map function   
+def triple_and_filter(lst):
+    return list(filter(lambda x: x % 4 == 0, map(lambda x: x*3, lst)))
     
 print(triple_and_filter([1, 2, 3, 4]))
