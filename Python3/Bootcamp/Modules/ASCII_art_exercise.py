ae5ad6fff2279d87d4c6044fa9b064795d069f51
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-23 00:17:57 -0700

    Make print_art(..) function

--- Python3/Bootcamp/Modules/ASCII_art_exercise.py
+++ Python3/Bootcamp/Modules/ASCII_art_exercise.py
@@ -1,14 +1,18 @@
-import pyfiglet
+from pyfiglet import figlet_format
 from termcolor import colored
 
-valid_colors = (red, green, yellow, blue, magenta, cyan, white)
+def print_art(msg, color):
+
+    valid_colors = ("red", "green", "yellow", "blue", "magenta", "cyan", "white")
+
+    if color not in valid_colors:
+        color = "blue"
+    
+    ascii_art = figlet_format(msg)
+    colored_ascii = colored(ascii_art, color = color)
+    print(colored_ascii)
+    
 
 msg = input("What would you like to print? ")
 color = input("What color? ")
-
-if color not in valid_colors:
-    color = "blue"
-
-ascii_art = pyfiglet.figlet_format(msg)
-colored_ascii = colored(ascii_art, color = color)
-print(colored_ascii)
+print_art(msg, color)
