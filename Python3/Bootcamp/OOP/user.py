999b3418369528dcda19eec43253a22245c836bd
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-23 21:25:11 -0700

    Add attributes first, last, and age

--- Python3/Bootcamp/OOP/user.py
+++ Python3/Bootcamp/OOP/user.py
@@ -1,5 +1,11 @@
 class User:
-    pass
-    
-user1 = User()
-print(type(user1))
+    def __init__(self, first):
+        self.first = first
+        self.last = last
+        self.age = age
+
+user1 = User("Joe", "Dawson", 25)
+user2 = User("Jack", "Chap", 29)
+
+print(user1.first, user1.last)
+print(user2.first, user2.last)
