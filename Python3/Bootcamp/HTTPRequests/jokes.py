4e742e6003d3bf10e9452186e35d2c8ee566da17
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-23 19:29:05 -0700

    Use python else-if statement

--- Python3/Bootcamp/HTTPRequests/jokes.py
+++ Python3/Bootcamp/HTTPRequests/jokes.py
@@ -19,7 +19,7 @@ results = res["results"]
 if num_jokes > 1:
     print(f"I found {num_jokes} about {term}.  Here's one:")
     print(choice(results))
-else if num_jokes == 1:
+elif num_jokes == 1:
     print(f"I found one about {term}.  Here's one:")
     print(results[0]["joke"])
 else:
