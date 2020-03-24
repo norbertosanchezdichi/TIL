f49974f9fe2dd63f97c18a7cd0e05fac1ba3afc2
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-23 23:54:48 -0700

    Add class attribute.  Define logout().

--- Python3/Bootcamp/OOPPartI/user.py
+++ Python3/Bootcamp/OOPPartI/user.py
@@ -1,8 +1,16 @@
 class User:
+    
+    active_users = 0
+    
     def __init__(self, first, last, age):
         self.first = first
         self.last = last
         self.age = age
+        User.active_users += 1
+        
+    def logout(self):
+        User.active_users -= 1
+        return f"{self.first} has logged out"
 
     def full_name(self):
         return f"{self.first} {self.last}"
@@ -23,8 +31,12 @@ class User:
 user1 = User("Joe", "Dawson", 25)
 user2 = User("Jack", "Chap", 29)
 
+print(User.active_users)
+
 print(user1.full_name())
 print(user2.full_name())
+
+print(User.active_users)
 
 print(user1.likes("Ice Cream"))
 print(user2.likes("Chips"))
@@ -35,3 +47,7 @@ print(user2.initials())
 print(user1.is_senior())
 print(user1.birthday())
 print(user1.age)
+
+print(user2.logout())
+
+print(User.active_users)
