83d44186a3010567ac67da3597d9eb24e398b6ce
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-25 22:11:05 -0700

    Initial commi

---
+++ Python3/Bootcamp/Exercises/DeckOfCards/Card.py
@@ -0,0 +1,8 @@
+def Card:
+
+    def __init__(self, suit, value):
+        self.suit = suit
+        self.value = value
+        
+    def __repr__(self):
+        return f"{self.value} of {self.suit}"
