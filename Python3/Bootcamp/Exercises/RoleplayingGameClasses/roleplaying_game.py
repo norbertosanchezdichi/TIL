e9cc4a33e79cecb1b2340928c40a95dc774aa568
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-26 22:44:57 -0700

    Add attributes to parent __init__

---
+++ Python3/Bootcamp/Exercises/RoleplayingGameClasses/roleplaying_game.py
@@ -0,0 +1,29 @@
+class Character:
+    def __init__(self, name, hp, level):
+        self.name = name
+        self.hp = hp
+        self.level = level
+        
+    @property
+    def name(self):
+        return self.name
+        
+    @property
+    def hp(self):
+        return self.hp
+        
+    @property
+    def level(self):
+        return self.level
+        
+class NPC(Character):
+    def __init__(self, name, hp, level):
+        super().__init__(name, hp, level)
+    def speak(self):
+        return f"I heard there were monsters running around last night!"
+        
+villager = NPC("Bob", 100, 12)
+print(villager.name)
+print(villager.hp)
+print(villager.level)
+print(villager.speak())
