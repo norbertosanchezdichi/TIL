4e37a5c6121d67fee30dc73d5e62d83f0c352dfc
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-26 22:12:14 -0700

    Use super()

--- Python3/Bootcamp/OOPPartII/inheritance.py
+++ Python3/Bootcamp/OOPPartII/inheritance.py
@@ -1,16 +1,34 @@
 class Animal:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        
+    def __repr__(self):
+        return f"{self.name} is a {self.species}"
+    
     def make_sound(self, sound):
         print(sound)
-        
-    cool = True
     
 class Cat(Animal):
-    pass
+    def __init__(self, name, species, breed, toy):
+        super().__init__(name, species = "Cat")
+        self.breed = breed
+        self.toy = toy
+        
+    def play(self):
+        print(f"{self.name} plays with {self.toy}")
     
-gandalf = Cat()
-gandalf.make_sound("meow")
-gandalf.cool = True
-print(gandalf.cool)
-print(Cat.cool)
-print(Animal.cool)
-print(isinstance(gandalf, Animal))
+#gandalf = Cat()
+#gandalf.make_sound("meow")
+#gandalf.cool = True
+#print(gandalf.cool)
+#print(Cat.cool)
+#print(Animal.cool)
+#print(isinstance(gandalf, Animal))
+
+blue = Cat("Blue", "Scottish Fold", "String")
+print(blue)
+print(blue.species)
+print(blue.breed)
+print(blue.toy)
+print(blue.play())
