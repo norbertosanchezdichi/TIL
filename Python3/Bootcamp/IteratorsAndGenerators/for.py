192e1e6758e27f80d6a626aab5e977adb905e631
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-28 00:48:37 -0700

    Fix typo

--- Python3/Bootcamp/IteratorsAndGenerators/for.py
+++ Python3/Bootcamp/IteratorsAndGenerators/for.py
@@ -2,7 +2,7 @@ def my_for(iterable, func):
     iterator = iter(iterable)
     while True:
         try:
-            think = next(iterator)
+            thing = next(iterator)
         except StopIteration:
             break
         else:
