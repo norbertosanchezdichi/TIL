ba37f7810ce8c2bb03dd77e73c3b2220474cc541
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-28 00:16:28 -0700

    Remove print in len function to avoid TypeError

--- Python3/Bootcamp/Exercises/SpecialMethodsTrain/Train.py
+++ Python3/Bootcamp/Exercises/SpecialMethodsTrain/Train.py
@@ -6,7 +6,7 @@ class Train:
         return f"{self.num_cars} car train"
         
     def __len__(self):
-        print(self.num_cars)
+        return self.num_cars
         
 a_train = Train(4)
 print(a_train)
