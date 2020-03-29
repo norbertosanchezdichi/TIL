24209fecfcd1c3cec3563774799beaca4128efa7
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-29 15:35:50 -0700

    Fix SyntaxError

--- Python3/Bootcamp/Exercises/OnlyInts/only_ints.py
+++ Python3/Bootcamp/Exercises/OnlyInts/only_ints.py
@@ -12,7 +12,7 @@ def only_ints(fn):
 def only_ints(fn):
     @wraps(fn)
     def wrapper(*args, **kwargs):
-        if any([arg for arg in args if type(arg) != int] or if len(kwargs)):
+        if any([arg for arg in args if type(arg) != int]) or if len(kwargs):
             return "Please only invoke with integers."
         return fn(*args, **kwargs)
     return wrapper
