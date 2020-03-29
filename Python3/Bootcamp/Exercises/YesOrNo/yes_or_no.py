819be97c4c8d2ab192cac916d02c660b83180c82
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-28 17:38:56 -0700

    Add method that does not do math

--- Python3/Bootcamp/Exercises/YesOrNo/yes_or_no.py
+++ Python3/Bootcamp/Exercises/YesOrNo/yes_or_no.py
@@ -6,6 +6,12 @@ def yes_or_no():
         else:
             yield 'yes'
         count += 1
+        
+def yes_or_no():
+    answer = "yes"
+    while True:
+        yield answer
+        answer = "no" if answer == "yes" else "yes"
             
 gen = yes_or_no()
 print(next(gen)) # 'yes'
