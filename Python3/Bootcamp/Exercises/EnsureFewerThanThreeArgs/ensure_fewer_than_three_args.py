11cba3b718f0d83ca43284732f00e22649940a7a
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-29 15:05:35 -0700

    Initial commit

---
+++ Python3/Bootcamp/Exercises/EnsureFewerThanThreeArgs/ensure_fewer_than_three_args.py
@@ -0,0 +1,18 @@
+from functools import wraps
+
+def ensure_fewer_than_three_args(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        if len(args) < 3:
+            return fn(*args, **kwargs)
+        return f"Too many arguments!"
+
+@ensure_fewer_than_three_args
+def add_all(*nums):
+    return sum(nums)
+
+add_all() # 0
+add_all(1) # 1
+add_all(1,2) # 3
+add_all(1,2,3) # "Too many arguments!"
+add_all(1,2,3,4,5,6) # "Too many arguments!"
