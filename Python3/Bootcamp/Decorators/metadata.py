c4a5187b0d24cb6324bea8d2819618653300d472
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-28 23:57:28 -0700

    Add necessary indentation

--- Python3/Bootcamp/Decorators/metadata.py
+++ Python3/Bootcamp/Decorators/metadata.py
@@ -1,10 +1,10 @@
 def log_function_data(fn):
     def wrapper(*args, **kwargs):
-    """I AM WRAPPER FUNCTION"""
-    print(f"You're about to call {fn.__name__}")
-    print(f"Here's the documentation: {fn.__doc__}")
-    return fn(*args, **kwargs)
-return wrapper
+        """I AM WRAPPER FUNCTION"""
+        print(f"You're about to call {fn.__name__}")
+        print(f"Here's the documentation: {fn.__doc__}")
+        return fn(*args, **kwargs)
+    return wrapper
 
 @log_function_data
 def add(x, y):
