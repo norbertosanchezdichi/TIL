04a119b361c957aaa09afa60aefafc2c4b5ae66d
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-28 21:19:02 -0700

    Use correct variable that contains iteratable index

--- Python3/Bootcamp/Exercises/MakeSong/make_song.py
+++ Python3/Bootcamp/Exercises/MakeSong/make_song.py
@@ -1,11 +1,11 @@
 def make_song(verses = 99, beverage = "soda"):
     for num in range(verses, -1, -1):
-        if verses == 0:
+        if num == 0:
             yield f"No more {beverage}!"
-        elif verses == 1:
-            yield f"Only {verses} bottle of {beverage} left!"
+        elif num == 1:
+            yield f"Only {num} bottle of {beverage} left!"
         else:    
-            yield f"{verses} bottles of {beverage} on the wall."
+            yield f"{num} bottles of {beverage} on the wall."
 
 song = make_song(4, "michelada")
 print(next(song))
