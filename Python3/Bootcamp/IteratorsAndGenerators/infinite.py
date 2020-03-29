135b4c1a8a88e307ceafdd9ab5ebf1ae6ee732ca
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-28 17:48:16 -0700

    Use object beat instead of calling function over again

--- Python3/Bootcamp/IteratorsAndGenerators/infinite.py
+++ Python3/Bootcamp/IteratorsAndGenerators/infinite.py
@@ -2,14 +2,15 @@ def current_beat():
     nums = (1, 2, 3, 4)
     i = 0
     while True:
-        if i>= len(nums) : i = 0
+        if i >= len(nums) : i = 0
         yield nums[i]
         i += 1
 
-print(next(current_beat()))
-print(next(current_beat()))
-print(next(current_beat()))
-print(next(current_beat()))
-print(next(current_beat()))
-print(next(current_beat()))
-print(next(current_beat()))
+beat = current_beat()
+print(next(beat))
+print(next(beat))
+print(next(beat))
+print(next(beat))
+print(next(beat))
+print(next(beat))
+print(next(beat))
