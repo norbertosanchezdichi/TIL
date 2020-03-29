e7ebae6488d65ad7a49530d1ac28e186a0266f96
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-28 22:08:11 -0700

    Add print statements for next iteration

--- Python3/Bootcamp/Exercises/GetMultiples/get_multiples.py
+++ Python3/Bootcamp/Exercises/GetMultiples/get_multiples.py
@@ -2,3 +2,10 @@ def get_multiples(num = 1, multiples = 10):
     for multiple in range(1, multiples + 1):
         yield num * multiple
         
+two = get_multiples(2, 5)
+print(next(two))
+print(next(two))
+print(next(two))
+print(next(two))
+print(next(two))
+print(next(two))
