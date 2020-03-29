6f6cab4bfbe4694dc466179d62535897b745b653
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-29 16:00:15 -0700

    Close index bracket at the correct location

--- Python3/Bootcamp/Decorators/ensure_first_arg.py
+++ Python3/Bootcamp/Decorators/ensure_first_arg.py
@@ -4,7 +4,7 @@ def ensure_first_arg_is(val):
     def inner(fn):
         @wraps(fn)
         def wrapper(*args, **kwargs):
-            if args and args[0 != val]:
+            if args and args[0] != val:
                 return f"First arg needs to be {val}"
             return fn(*args, **kwargs)
         return wrapper
