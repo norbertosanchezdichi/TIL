6512f98a9ca49583be7f16cec09bb39eb0794506
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-29 14:06:13 -0700

    Add comparison between a generator and a list sum.

--- Python3/Bootcamp/Decorators/timing.py
+++ Python3/Bootcamp/Decorators/timing.py
@@ -7,12 +7,17 @@ def speed_test(fn):
         start_time = time()
         result = fn(*args, **kwargs)
         end_time = time()
+        print(f"Executing {fn.__name__}")
         print(f"Time Elapsed: {end_time - start_time}")
         return result
     return wrapper
     
 @speed_test
-def sum_nums():
-    return sum(x for x in range(1000000))
+def sum_nums_gen(number):
+    return sum(x for x in range(number))
     
-print(sum_nums())
+def sum_nums_list(number):
+    return sum([x for x in range(number)])
+    
+print(sum_nums_gen(50000000))
+print(sum_nums_list(50000000))
