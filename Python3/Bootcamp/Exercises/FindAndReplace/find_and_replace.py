9f86eeb8d5e4e779508e52598d21147808012ce2
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-30 00:40:41 -0700

    Replace other pronouns

--- Python3/Bootcamp/Exercises/FindAndReplace/find_and_replace.py
+++ Python3/Bootcamp/Exercises/FindAndReplace/find_and_replace.py
@@ -9,3 +9,6 @@ def find_and_replace(file_name, search, replace):
 find_and_replace("story.txt", "Alice", "Norberto")
 find_and_replace("story.txt", "she", "he")
 find_and_replace("story.txt", "She", "He")
+find_and_replace("story.txt", "her", "his")
+find_and_replace("story.txt", "Her", "His")
+find_and_replace("story.txt", "herself", "himself")
