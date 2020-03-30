ec5ef99bc84b923293352d779982dfc15c5df265
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-03-29 21:15:32 -0700

    Use seek(..), readline(..), and readlines(..)

--- Python3/Bootcamp/FileIO/reading_files.py
+++ Python3/Bootcamp/FileIO/reading_files.py
@@ -1,2 +1,16 @@
 file = open("story.txt")
-print(file.read()) 
+
+print(file.read())
+print(file.seek(0))
+print(file.read())
+
+print(file.seek(0))
+print(file.readline())
+print(file.readline())
+
+print(file.seek(0))
+print(file.readlines())
+
+print(file.closed)
+print(file.close())
+print(file.closed)
