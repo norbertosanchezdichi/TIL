062e92dce77801083e780b8cc36570293f3f60f4
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-04-05 18:44:00 -0700

    Add missing ')'

--- Python3/Bootcamp/CSVAndPickling/writer.py
+++ Python3/Bootcamp/CSVAndPickling/writer.py
@@ -1,5 +1,5 @@
 from csv import writer
 with open("fighters_new.csv", "w") as file:
     csv_writer = writer(file)
-    csv_writer.writerow(["Character", "Move"]
+    csv_writer.writerow(["Character", "Move"])
     csv_writer.writerow(["Ryu", "Hadouken"])
