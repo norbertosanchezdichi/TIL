3b05793766ad52a87aa0bc3e0a1a750fc756db25
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-04-05 18:16:59 -0700

    Name row as 'figher'

--- Python3/Bootcamp/CSVAndPickling/reader.py
+++ Python3/Bootcamp/CSVAndPickling/reader.py
@@ -2,5 +2,5 @@ from csv import reader
 with open("fighters.csv") as file:
     csv_reader = reader(file)
     next(csv_reader)
-    for row in csv_reader:
+    for fighter in csv_reader:
         print(f"{fighter[0]} is from {fighter[1]}")
