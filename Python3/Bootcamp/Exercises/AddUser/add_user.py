ce38c0a205085585fe62dea66349b28a30d39a70
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-04-06 13:02:26 -0700

    Copy csv file before adding user

--- Python3/Bootcamp/Exercises/AddUser/add_user.py
+++ Python3/Bootcamp/Exercises/AddUser/add_user.py
@@ -7,6 +7,13 @@ def add_user(first_name, last_name):
     
         with open("users.csv", "w") as file:
             csv_writer = DictWriter(file, fieldnames = headers)
+            
+            for user in csv_reader:
+                csv_writer.writerow({
+                    "First Name": user["First Name"],
+                    "Last Name": user["Last Name"]
+                    })
+            
             csv_writer.writerow({
                 "First Name": first_name,
                 "Last Name": last_name
