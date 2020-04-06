ee48769bfb1c4766c54ce2188992cbdf728842f4
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-04-06 12:52:38 -0700

    Use 'headers' variable to store headers

--- Python3/Bootcamp/Exercises/AddUser/add_user.py
+++ Python3/Bootcamp/Exercises/AddUser/add_user.py
@@ -3,9 +3,10 @@ from csv import DictReader, DictWriter
 def add_user(first_name, last_name):
     with open("users.csv") as file:
         csv_reader = DictReader(file)
+        headers = list(next(csv_reader))
     
         with open("users.csv", "w") as file:
-            csv_writer = DictWriter(file, fieldnames = next(csv_reader))
+            csv_writer = DictWriter(file, fieldnames = headers)
             csv_writer.writerow({
                 "First Name": first_name,
                 "Last Name": last_name
