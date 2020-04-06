a6ebb26059363269e4ec16132041c98bf63244e3
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-04-05 19:04:31 -0700

    Copy from fighters.csv and write new file with uppercase

--- Python3/Bootcamp/CSVAndPickling/writer.py
+++ Python3/Bootcamp/CSVAndPickling/writer.py
@@ -1,5 +1,14 @@
-from csv import writer
+from csv import reader, writer
 with open("fighters_new.csv", "w") as file:
     csv_writer = writer(file)
     csv_writer.writerow(["Character", "Move"])
     csv_writer.writerow(["Ryu", "Hadouken"])
+    
+with open('fighters.csv') as file:
+    csv_reader = reader(file)
+    fighters = [[s.upper() for s in row] for row in csv_reader]
+    
+with open('screaming_fighters.csv', 'w') as file:
+    csv_writer = writer(file)
+    for fighter in fighters:
+        csv_writer.writenow(fighter)
