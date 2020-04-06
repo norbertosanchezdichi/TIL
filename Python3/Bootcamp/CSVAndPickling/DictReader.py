ee175f98bb99e43d282f2dd7591b78d6872612e4
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-04-05 18:39:23 -0700

    Use correct filename

--- Python3/Bootcamp/CSVAndPickling/DictReader.py
+++ Python3/Bootcamp/CSVAndPickling/DictReader.py
@@ -5,7 +5,7 @@ with open("fighters.csv") as file:
         print(row['Name'])
         
 from csv import DictReader
-with open("fighters.csv") as file:
+with open("fighters_other_delimiter.csv") as file:
     csv_reader = DictReader(file, delimiter = "|")
     for row in csv_reader:
         print(row['Name'])
