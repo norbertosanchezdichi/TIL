e87f2c681947cb921e8753daea4056f27f2a9aa4
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-04-05 21:47:10 -0700

    Correct spelling

--- Python3/Bootcamp/CSVAndPickling/DictWriter.py
+++ Python3/Bootcamp/CSVAndPickling/DictWriter.py
@@ -1,7 +1,7 @@
 from csv import DictWriter
 with open("fighters_using_DictWriter.csv", "w") as file:
     headers = ["Character", "Move"]
-    csv_writer = DictWriter(file, filednames = headers)
+    csv_writer = DictWriter(file, fieldnames = headers)
     csv_writer.writeheader()
     csv_writer.writerow({
         "Character": "Ryu",
