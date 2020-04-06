40c3fdd8d8060068df3f7ad62a0a6e9c9051e776
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-04-06 13:13:33 -0700

    Use users variable to obtain header information

--- Python3/Bootcamp/Exercises/AddUser/add_user.py
+++ Python3/Bootcamp/Exercises/AddUser/add_user.py
@@ -4,8 +4,7 @@ def add_user(first_name, last_name):
     with open("users.csv") as file:
         csv_reader = DictReader(file)
         users = list(csv_reader)
-        headers = list(next(csv_reader))
-        print(headers)
+        headers = users[0]
     
         with open("users.csv", "w") as file:
             csv_writer = DictWriter(file, fieldnames = headers)
