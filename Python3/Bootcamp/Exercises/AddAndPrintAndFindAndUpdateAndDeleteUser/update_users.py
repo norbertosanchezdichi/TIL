69a723f39b5fa77cb0a9dee71867b2a6c1b04e81
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-04-06 16:31:52 -0700

    Initial commit

---
+++ Python3/Bootcamp/Exercises/AddAndPrintAndFindAndUpdateAndDeleteUser/update_users.py
@@ -0,0 +1,19 @@
+from csv import reader, writer
+
+def update_users(old_first, old_last, new_first, new_last):
+    with open("users.csv") as file:
+        users = list(reader(file))
+        count = 0
+        
+        with open("users.csv", "w") as file:
+            csv_writer = csv.writer(file)
+            for u in users:
+                if u[0] == old_first and u[1] == old_last:
+                    csv_writer.writerow([new_first, new_last])
+                    count += 1
+                else:
+                    csv_writer.writerow(u)
+                    
+    return f"Users updated: {count}"
+                    
+print(update_users("Norberto", "Sanchez-Dichi", "Barack", "Obama"))
