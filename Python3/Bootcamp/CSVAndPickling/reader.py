c2900825ce48835e9f1a06d52ce4e19605cdb431
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-04-05 18:15:42 -0700

    Initial commit

---
+++ Python3/Bootcamp/CSVAndPickling/reader.py
@@ -0,0 +1,6 @@
+from csv import reader
+with open("fighters.csv") as file:
+    csv_reader = reader(file)
+    next(csv_reader)
+    for row in csv_reader:
+        print(f"{fighter[0]} is from {fighter[1]}")
