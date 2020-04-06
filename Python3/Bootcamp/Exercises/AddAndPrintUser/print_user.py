ca7a391ddccd42815f12a1e99494456583487896
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-04-06 13:40:53 -0700

    Initial commit

---
+++ Python3/Bootcamp/Exercises/AddAndPrintUser/print_user.py
@@ -0,0 +1,12 @@
+from csv import DictReader
+
+def print_users():
+    with open("users.csv") as file:
+        csv_reader = DictReader(file)
+        next(csv_reader)
+        users = list(csv_reader)
+        
+        for u in users:
+            print(f"{u['First Name'] u['Last Name']}")
+            
+print_users()
