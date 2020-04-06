e434b5d2b775cb1dee1c33bb660bdb6a191a6465
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-04-05 18:38:28 -0700

    Correct spelling of 'delimiter'

--- Python3/Bootcamp/CSVAndPickling/DictReader.py
+++ Python3/Bootcamp/CSVAndPickling/DictReader.py
@@ -6,6 +6,6 @@ with open("fighters.csv") as file:
         
 from csv import DictReader
 with open("fighters.csv") as file:
-    csv_reader = DictReader(file, delimeter = "|")
+    csv_reader = DictReader(file, delimiter = "|")
     for row in csv_reader:
         print(row['Name'])
