bf9367733ba5402d53ff90d719504e639ef13927
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-04-06 23:34:11 -0700

    Convert code to character

--- Python3/Bootcamp/Exercises/MagicKeyboardScraping/magic_keyboard_scraping.py
+++ Python3/Bootcamp/Exercises/MagicKeyboardScraping/magic_keyboard_scraping.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from csv import writer
 
-response = requests.get("https://www.apple.com/shop/product/MXQT2LL/A/magic-keyboard-for-ipad-pro-11%E2%80%91inch-2nd-generation-us-english")
+response = requests.get("https://www.apple.com/shop/product/MXQT2LL/A/magic-keyboard-for-ipad-pro-11-inch-2nd-generation-us-english")
 
 print(response.text)
 
