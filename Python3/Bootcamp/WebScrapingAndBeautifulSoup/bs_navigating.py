97ec06c1f346c9630d427e29d5d802d75e15bc72
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-04-06 22:04:01 -0700

    Initial commit

---
+++ Python3/Bootcamp/WebScrapingAndBeautifulSoup/bs_navigating.py
@@ -0,0 +1,50 @@
+from bs4 import BeautifulSoup
+
+html = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>First HTML Page</title>
+</head>
+<body>
+  <div id="first">
+    <h3 data-example="yes">hi</h3>
+    <p>more text.</p>
+  </div>
+  <ol>
+    <li class="special super-special">This list item is special.</li>
+    <li class="special">This list item is also special.</li>
+    <li>This list item is not special.</li>
+  </ol>
+  <div data-example="yes">bye</div>
+</body>
+</html>
+"""
+
+soup = BeautifulSoup(html, "html.parser")
+
+# Navigating Via Tags
+data = soup.body.contents[1].contents
+print(data)
+
+data = soup.body.contents[1].next_sibling.next_sibling
+print(data)
+
+data = soup.find(class_ = "super-special").parent
+print(data)
+
+# Navigating Via Searching
+data = soup.find(id = "first").find_next_sibling()
+print(data)
+
+data = soup.find(id = "first").find_next_sibling().next_sibling()
+print(data)
+
+data = soup.select("[data-example]")[1].find_previous_sibling()
+print(data)
+
+data = soup.find(class_ = "super-special").find_next_sibling(class_ = "special")
+print(data)
+
+data = soup.find("h3").find_parent("html")
