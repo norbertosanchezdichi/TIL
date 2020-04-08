5b45ca4ddb0b6aa7b9e480d3fcaeecd9dca283be
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-04-07 23:29:24 -0700

    Initial commit

---
+++ Python3/Bootcamp/Projects/WebScrapingProject/scraper.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+from time import sleep
+
+base_url = "http://quotes.toscrape.com"
+url = "/page/1"
+
+while url:
+    res = requests.get(f"{base_url}{url}")
+    print(f"Now scraping {base_url}{url}...")
+    soup = BeautifulSoup(res, "html.parser")
+    quotes = soup.find_all(class_"quote")
+    
+    all_quotes = {}
+    for quote in quotes:
+        all_quotes.append({
+        "text": quote.find(class_ = "text").get_text(),
+        "author": quote.find(class_ = "author").get_text(),
+        "bio-link": quote.find("a")["href"]
+        })
+        
+    next_btn = soup.find(class_="next")
+    url = next_btn.find("a")["href"] if next_btn else None
+    sleep(1)
+print(all_quotes)
