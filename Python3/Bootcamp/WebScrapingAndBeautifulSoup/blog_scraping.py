0a6b5458d839965bef61d953ab85adb5ccd777be
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-04-07 22:15:00 -0700

    Write to file

--- Python3/Bootcamp/WebScrapingAndBeautifulSoup/blog_scraping.py
+++ Python3/Bootcamp/WebScrapingAndBeautifulSoup/blog_scraping.py
@@ -17,4 +17,4 @@ with open("blog_data.csv", "w") as csv_file:
         title = a_tag.get_text()
         url = a_tag['href']
         date = article.find("time")["datetime"]
-        csv_writer.writerow([title, url, date])        
+        csv_writer.writerow([title, url, date])
