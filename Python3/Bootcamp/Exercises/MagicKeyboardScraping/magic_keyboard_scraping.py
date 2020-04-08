ba550dbac12987ec8c2e75309c61ff60ceeff200
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-04-07 22:26:25 -0700

    Change approach on how to check if available

--- Python3/Bootcamp/Exercises/MagicKeyboardScraping/magic_keyboard_scraping.py
+++ Python3/Bootcamp/Exercises/MagicKeyboardScraping/magic_keyboard_scraping.py
@@ -1,16 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
-from csv import reader
+from fileds import reader
 
 #response = requests.get("https://www.apple.com/shop/product/MXQT2LL/A/magic-keyboard-for-ipad-pro-11-inch-2nd-generation-us-english")
 
-with open("scratchpad.html") as file:
-    data = reader(file)
-    soup = BeautifulSoup(str(data), "html.parser")
+file = open("scratchpad.html")
+soup = BeautifulSoup(file, "html.parser")
+availability = soup.findAll(class_="as-purchaseinfo-dudeinfo-deliverymsg")
 
-li = soup.find(class_ = "as-purchaseinfo-availabilityinfo row")
-div1 = li.find(class_ = "as-pdp-deliverydates column large-6 small-6 isdudeavailable as-purchaseinfo-dudeinfo as-icondetails as-icondetails-topicon")
-div2 = div1.find(class_ = "shipdeliverydates as-pdp-shipdeliverydates")
-
-#with open("juicy_details.txt", "w") as file:
-#    file.write()
+with open("juicy_details.txt", "w") as file:
+    file.write("Currently unavailable" not in availability)
