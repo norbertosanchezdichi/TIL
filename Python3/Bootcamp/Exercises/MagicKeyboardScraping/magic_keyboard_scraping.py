4072d5fb1fd1d98ac10fc9f3abf187bf8f0f9e92
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-04-07 22:14:36 -0700

    Use scratchpad.html

--- Python3/Bootcamp/Exercises/MagicKeyboardScraping/magic_keyboard_scraping.py
+++ Python3/Bootcamp/Exercises/MagicKeyboardScraping/magic_keyboard_scraping.py
@@ -1,14 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
-from csv import writer
+from csv import reader
 
-response = requests.get("https://www.apple.com/shop/product/MXQT2LL/A/magic-keyboard-for-ipad-pro-11-inch-2nd-generation-us-english")
+#response = requests.get("https://www.apple.com/shop/product/MXQT2LL/A/magic-keyboard-for-ipad-pro-11-inch-2nd-generation-us-english")
 
-print(response.text)
+with open("scratchpad.html") as file:
+    data = reader(file)
+    soup = BeautifulSoup(str(data), "html.parser")
 
-soup = BeautifulSoup(response.text, "html.parser")
+li = soup.find(class_ = "as-purchaseinfo-availabilityinfo row")
+div1 = li.find(class_ = "as-pdp-deliverydates column large-6 small-6 isdudeavailable as-purchaseinfo-dudeinfo as-icondetails as-icondetails-topicon")
+div2 = div1.find(class_ = "shipdeliverydates as-pdp-shipdeliverydates")
 
-
-
-
-
+#with open("juicy_details.txt", "w") as file:
+#    file.write()
