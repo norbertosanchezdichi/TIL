bf18389f755fcecbe35230fede62295666e0ba74
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-04-07 22:27:32 -0700

    Remove import of reader/writer

--- Python3/Bootcamp/Exercises/MagicKeyboardScraping/magic_keyboard_scraping.py
+++ Python3/Bootcamp/Exercises/MagicKeyboardScraping/magic_keyboard_scraping.py
@@ -1,6 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-from fileds import reader
 
 #response = requests.get("https://www.apple.com/shop/product/MXQT2LL/A/magic-keyboard-for-ipad-pro-11-inch-2nd-generation-us-english")
 
