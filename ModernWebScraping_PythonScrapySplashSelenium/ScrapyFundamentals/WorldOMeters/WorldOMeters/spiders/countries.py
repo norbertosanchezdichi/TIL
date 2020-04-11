4b083bee80482146943a757890b328e4337a6f87
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-04-10 19:54:53 -0700

    Edit urls for compatibility with Scrapy

--- ModernWebScraping_PythonScrapySplashSelenium/ScrapyFundamentals/WorldOMeters/WorldOMeters/spiders/countries.py
+++ ModernWebScraping_PythonScrapySplashSelenium/ScrapyFundamentals/WorldOMeters/WorldOMeters/spiders/countries.py
@@ -4,8 +4,8 @@ import scrapy
 
 class CountriesSpider(scrapy.Spider):
     name = 'countries'
-    allowed_domains = ['www.worldometers.info/world-population/population-by-country']
-    start_urls = ['http://www.worldometers.info/world-population/population-by-country/']
+    allowed_domains = ['www.worldometers.info/']
+    start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
 
     def parse(self, response):
         pass
