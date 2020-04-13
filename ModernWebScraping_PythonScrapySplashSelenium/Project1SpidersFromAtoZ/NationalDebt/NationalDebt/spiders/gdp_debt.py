a884b2f62a60a9941e959c299c5ac44ee47f5e6b
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-04-12 18:06:04 -0700

    Initial commit

--- ModernWebScraping_PythonScrapySplashSelenium/Project1SpidersFromAtoZ/NationalDebt/NationalDebt/spiders/gdp_debt.py
+++ ModernWebScraping_PythonScrapySplashSelenium/Project1SpidersFromAtoZ/NationalDebt/NationalDebt/spiders/gdp_debt.py
@@ -8,4 +8,14 @@ class GdpDebtSpider(scrapy.Spider):
     start_urls = ['http://www.worldpopulationreview.com/countries/countries-by-national-debt/']
 
     def parse(self, response):
-        pass
+        rows = response.xpath("//*[@id="__next"]/div/div[1]/div[2]/div[2]/div[1]/div/div/div/div/div[2]/table/tbody/tr[1]")
+        for row in rows:
+            country_name = row.xpath(".//td[1]/a/text()").get()
+            country_link = row.xpath(".//td[1]/a/@href").get()
+            national_debt_to_GDP = row.xpath(".//td[2]/text()").get()
+            
+            yield {
+                'country_name': country_name,
+                'country_link': country_link,
+                'national_debt_to_GDP': national_debt_to_GDP
+            }
