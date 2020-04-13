cc6e055d3394c438dd4d2ba47eb041e85f134725
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-04-12 18:43:46 -0700

    Truncate allowed_domain entry

--- ModernWebScraping_PythonScrapySplashSelenium/Project1SpidersFromAtoZ/NationalDebt/NationalDebt/spiders/gdp_debt.py
+++ ModernWebScraping_PythonScrapySplashSelenium/Project1SpidersFromAtoZ/NationalDebt/NationalDebt/spiders/gdp_debt.py
@@ -4,7 +4,7 @@ import scrapy
 
 class GdpDebtSpider(scrapy.Spider):
     name = 'gdp_debt'
-    allowed_domains = ['www.worldpopulationreview.com/countries/countries-by-national-debt']
+    allowed_domains = ['www.worldpopulationreview.com']
     start_urls = ['http://www.worldpopulationreview.com/countries/countries-by-national-debt/']
 
     def parse(self, response):
