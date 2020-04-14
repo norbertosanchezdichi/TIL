e06dcfe500a2de524db629d649ea183aa613fc29
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-04-13 21:09:52 -0700

    Fix AttributeError

--- ModernWebScraping_PythonScrapySplashSelenium/Project2DealingWithMultiplePages/TinyDeal/TinyDeal/spiders/special_offers.py
+++ ModernWebScraping_PythonScrapySplashSelenium/Project2DealingWithMultiplePages/TinyDeal/TinyDeal/spiders/special_offers.py
@@ -12,6 +12,6 @@ class SpecialOffersSpider(scrapy.Spider):
             yield {
                 'title': product.xpath(".//a[@class='p_box_title']/text()").get(),
                 'url': response.urljoin(product.xpath(".//a[@class='p_box_title']/@href").get()),
-                'discount_price': product.xpath(".//a[@class='p_box_price']/span[1]/text()").get_text(),
-                'original_price': product.xpath(".//a[@class='p_box_price']/span[2]/text()").get_text(),
+                'discount_price': product.xpath(".//a[@class='p_box_price']/span[1]/text()").get(),
+                'original_price': product.xpath(".//a[@class='p_box_price']/span[2]/text()").get(),
             }
