3f9d49ff125a5aea470ffad8f770500505cbd6e3
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-04-14 16:01:34 -0700

    Normalize space using normalize-space(..) function

--- ModernWebScraping_PythonScrapySplashSelenium/Project3BuildCrawlersUsingScrapy/IMDB/IMDB/spiders/best_movies.py
+++ ModernWebScraping_PythonScrapySplashSelenium/Project3BuildCrawlersUsingScrapy/IMDB/IMDB/spiders/best_movies.py
@@ -17,7 +17,7 @@ class BestMoviesSpider(CrawlSpider):
         yield {
             'title': response.xpath("//div[@class='title_wrapper']/h1/text()").get(),
             'year': response.xpath("//span[@id='titleYear']/a/text()").get(),
-            'duration': response.xpath("(//time)[1]/text()").get(),
+            'duration': response.xpath("normalize-space((//time)[1]/text())").get(),
             'genre': response.xpath("//div[@class='subtext']/a[1]/text()").get(),
             'rating': response.xpath("//span[@itemprop='ratingValue']/text()").get(),
             'url': response.url,
