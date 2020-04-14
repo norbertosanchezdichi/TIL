c96d59e66ddfd703e61257b8c920de4282875caf
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-04-13 20:33:14 -0700

    Modify url's. Use https.  Remove '/' since Scrapy automatically does this.

--- ModernWebScraping_PythonScrapySplashSelenium/Project2DealingWithMultiplePages/TinyDeal/TinyDeal/spiders/special_offers.py
+++ ModernWebScraping_PythonScrapySplashSelenium/Project2DealingWithMultiplePages/TinyDeal/TinyDeal/spiders/special_offers.py
@@ -4,8 +4,8 @@ import scrapy
 
 class SpecialOffersSpider(scrapy.Spider):
     name = 'special_offers'
-    allowed_domains = ['www.tinydeal.com.hk/specials.html']
-    start_urls = ['http://www.tinydeal.com.hk/specials.html/']
+    allowed_domains = ['www.tinydeal.com.hk']
+    start_urls = ['https://www.tinydeal.com.hk/specials.html']
 
     def parse(self, response):
         pass
