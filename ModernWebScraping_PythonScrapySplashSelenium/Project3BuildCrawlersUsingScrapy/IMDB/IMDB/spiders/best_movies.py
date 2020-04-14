2fe4168eff1941c10d537a2bf295d2b5f7890063
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-04-14 15:54:40 -0700

    Set up parse

--- ModernWebScraping_PythonScrapySplashSelenium/Project3BuildCrawlersUsingScrapy/IMDB/IMDB/spiders/best_movies.py
+++ ModernWebScraping_PythonScrapySplashSelenium/Project3BuildCrawlersUsingScrapy/IMDB/IMDB/spiders/best_movies.py
@@ -7,15 +7,18 @@ from scrapy.spiders import CrawlSpider, Rule
 class BestMoviesSpider(CrawlSpider):
     name = 'best_movies'
     allowed_domains = ['imdb.com']
-    start_urls = ['http://imdb.com/']
+    start_urls = ['https://www.imdb.com/search/title/?groups=top_250&sort=user_rating']
 
     rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        Rule(restrict_xpaths="//h3[@class='lister-item-header']/a", callback='parse_item', follow=True),
     )
 
     def parse_item(self, response):
-        item = {}
-        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
-        #item['name'] = response.xpath('//div[@id="name"]').get()
-        #item['description'] = response.xpath('//div[@id="description"]').get()
-        return item
+        yield {
+            'title': response.xpath("//div[@class='title_wrapper']/h1/text()").get(),
+            'year': response.xpath("//span[@id='titleYear']/a/text()").get(),
+            'duration': response.xpath("(//time)[1]/text()").get(),
+            'genre': response.xpath("//div[@class='subtext']/a[1]/text()").get(),
+            'rating': response.xpath("//span[@itemprop='ratingValue']/text()").get(),
+            'url': response.url,
+        }
