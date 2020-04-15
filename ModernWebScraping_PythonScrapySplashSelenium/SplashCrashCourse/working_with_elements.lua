c556034e06db0f49d9f94d214895e029dd3e5d1c
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-04-15 03:03:15 -0700

    Replace indentation with 4 spaces for clarity

--- ModernWebScraping_PythonScrapySplashSelenium/SplashCrashCourse/working_with_elements.lua
+++ ModernWebScraping_PythonScrapySplashSelenium/SplashCrashCourse/working_with_elements.lua
@@ -12,29 +12,29 @@ function main(splash, args)
     splash:on_request(function(request)
         request:set_header('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Safari/605.1.15')
     end)
-
-	url = args.url
-  assert(splash:go(url))
-  assert(splash:wait(1))
-  
-  input_box = assert(splash:select("#search_form_input_homepage"))
-	input_box:focus()
-  input_box:send_text("my user agent")
-  assert(splash:wait(0.5))
-  
-  --[[
-  search_button = assert(splash:select("#search_button_homepage"))
-  search_button:mouse_click()
-  assert(splash:wait(5))
-  --]]
-  
-  input_box:send_keys("<Enter>")
-  assert(splash:wait(5))
-  
-  splash:set_viewport_full()
-  
-  return {
+    
+    url = args.url
+    assert(splash:go(url))
+    assert(splash:wait(1))
+    
+    input_box = assert(splash:select("#search_form_input_homepage"))
+    input_box:focus()
+    input_box:send_text("my user agent")
+    assert(splash:wait(0.5))
+    
+    --[[
+    search_button = assert(splash:select("#search_button_homepage"))
+    search_button:mouse_click()
+    assert(splash:wait(5))
+    --]]
+    
+    input_box:send_keys("<Enter>")
+    assert(splash:wait(5))
+    
+    splash:set_viewport_full()
+    
+    return {
     image = splash:png(),
     html = splash:html()
-  }
-endd
+    }
+end
