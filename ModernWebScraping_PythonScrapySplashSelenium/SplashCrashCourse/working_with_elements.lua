b5aa8f9fe6f7408e0f9fd3cc0a34939d30f17e94
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-04-15 02:59:09 -0700

    Spoof User-Agent using a custom header

--- ModernWebScraping_PythonScrapySplashSelenium/SplashCrashCourse/working_with_elements.lua
+++ ModernWebScraping_PythonScrapySplashSelenium/SplashCrashCourse/working_with_elements.lua
@@ -1,5 +1,11 @@
 function main(splash, args)
-    splash:set_user_agent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Safari/605.1.15")
+    
+    --splash:set_user_agent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Safari/605.1.15")
+    
+    headers = {
+        ['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Safari/605.1.15'
+    }
+    splash:set_custom_headers(headers)
 
 	url = args.url
   assert(splash:go(url))
