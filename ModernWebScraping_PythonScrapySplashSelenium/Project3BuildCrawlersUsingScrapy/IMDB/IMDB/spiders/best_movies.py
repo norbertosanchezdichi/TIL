0673925c4006c010c77813e9155eebb007c0ee38
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-04-14 23:48:19 -0700

    Spoof User-Agent to Safari

--- ModernWebScraping_PythonScrapySplashSelenium/Project3BuildCrawlersUsingScrapy/IMDB/IMDB/spiders/best_movies.py
+++ ModernWebScraping_PythonScrapySplashSelenium/Project3BuildCrawlersUsingScrapy/IMDB/IMDB/spiders/best_movies.py
@@ -7,12 +7,23 @@ from scrapy.spiders import CrawlSpider, Rule
 class BestMoviesSpider(CrawlSpider):
     name = 'best_movies'
     allowed_domains = ['imdb.com']
-    start_urls = ['https://www.imdb.com/search/title/?groups=top_250&sort=user_rating']
+    
+    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Safari/605.1.15"
+    
+    def start_requests(self):
+        yield scrapy.Request(url='https://www.imdb.com/search/title/?groups=top_250&sort=user_rating', headers={
+            'User-Agent': self.user_agent
+            })
+        
 
     rules = (
-        Rule(LinkExtractor(restrict_xpaths="//h3[@class='lister-item-header']/a"), callback='parse_item', follow=True),
-        Rule(LinkExtractor(restrict_xpaths="(//a[@class='lister-page-next next-page'])[2]"))
+        Rule(LinkExtractor(restrict_xpaths="//h3[@class='lister-item-header']/a"), callback='parse_item', follow=True, process_request='set_user_agent'),
+        Rule(LinkExtractor(restrict_xpaths="(//a[@class='lister-page-next next-page'])[2]"), process_request='set_user_agent')
     )
+    
+    def set_user_agent(self, request):
+        request.headers['User-Agent'] = self.user_agent
+        return request
 
     def parse_item(self, response):
         yield {
@@ -22,4 +33,5 @@ class BestMoviesSpider(CrawlSpider):
             'genre': response.xpath("//div[@class='subtext']/a[1]/text()").get(),
             'rating': response.xpath("//span[@itemprop='ratingValue']/text()").get(),
             'url': response.url,
+            'User-Agnet': self.user_agent
         }
