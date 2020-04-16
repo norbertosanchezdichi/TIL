3791c8b851edbc00b6cb8ab59a621efcae8a462f
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-04-15 21:41:03 -0700

    Click on USD tab

--- ModernWebScraping_PythonScrapySplashSelenium/Project4ScrapingJavascriptWebsitesUsingSplash/splash_incognito_mode.lua
+++ ModernWebScraping_PythonScrapySplashSelenium/Project4ScrapingJavascriptWebsitesUsingSplash/splash_incognito_mode.lua
@@ -6,8 +6,8 @@ function main(splash, args)
   assert(splash:go(url))
 	assert(splash:wait(1))
   
-  rur_tab = assert(splash:select_all(".filterPanelItem___2z5Gb"))
-	rur_tab[3]:mouse_click()
+  usd_tab = assert(splash:select_all(".filterPanelItem___2z5Gb"))
+	usd_tab[3]:mouse_click()
   assert(splash:wait(1))
 	splash:set_viewport_full()
   
