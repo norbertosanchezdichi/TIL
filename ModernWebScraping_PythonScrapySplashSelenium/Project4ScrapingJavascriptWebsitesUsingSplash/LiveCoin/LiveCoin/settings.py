234df0f83502f44b91ae5599f3f447ffe963dfb3
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-04-15 21:39:30 -0700

    Enable Splash middleware

--- ModernWebScraping_PythonScrapySplashSelenium/Project4ScrapingJavascriptWebsitesUsingSplash/LiveCoin/LiveCoin/settings.py
+++ ModernWebScraping_PythonScrapySplashSelenium/Project4ScrapingJavascriptWebsitesUsingSplash/LiveCoin/LiveCoin/settings.py
@@ -52,9 +52,11 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'LiveCoin.middlewares.LivecoinDownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy_splash.SplashCookiesMiddleware': 723,
+    'scrapy_splash.SplashMiddleware': 725,
+    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
