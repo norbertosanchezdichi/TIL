fcc3be8851626109ee90d66c42501d2a318605aa
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-04-15 21:45:18 -0700

    Enable SplashDeduplicateArgsMiddleware to save disk space and save network traffic

--- ModernWebScraping_PythonScrapySplashSelenium/Project4ScrapingJavascriptWebsitesUsingSplash/LiveCoin/LiveCoin/settings.py
+++ ModernWebScraping_PythonScrapySplashSelenium/Project4ScrapingJavascriptWebsitesUsingSplash/LiveCoin/LiveCoin/settings.py
@@ -46,9 +46,9 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    'LiveCoin.middlewares.LivecoinSpiderMiddleware': 543,
-#}
+SPIDER_MIDDLEWARES = {
+    'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+}
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
