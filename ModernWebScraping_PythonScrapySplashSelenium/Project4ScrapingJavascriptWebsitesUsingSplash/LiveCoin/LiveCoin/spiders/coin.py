cfd21808a7433ca52e46e16fd979723265284185
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-04-15 22:23:27 -0700

    Integrate splash_scrapy.  Add 'splash_incognito_mode.lua' script

--- ModernWebScraping_PythonScrapySplashSelenium/Project4ScrapingJavascriptWebsitesUsingSplash/LiveCoin/LiveCoin/spiders/coin.py
+++ ModernWebScraping_PythonScrapySplashSelenium/Project4ScrapingJavascriptWebsitesUsingSplash/LiveCoin/LiveCoin/spiders/coin.py
@@ -1,11 +1,35 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from scrapy_splash import SplashRequest
 
 class CoinSpider(scrapy.Spider):
     name = 'coin'
     allowed_domains = ['www.livecoin.net/en']
     start_urls = ['http://www.livecoin.net/en/']
+    
+    script = '''
+        function main(splash, args)
+      
+            splash.private_mode_enabled = false
+          
+            url = args.url
+            assert(splash:go(url))
+            assert(splash:wait(1))
+          
+            usd_tab = assert(splash:select_all(".filterPanelItem___2z5Gb"))
+            usd_tab[3]:mouse_click()
+            assert(splash:wait(1))
+            splash:set_viewport_full()
+          
+            return splash:html()
+        
+        end
+    '''
+    
+    def start_requests(self):
+        yield SplashRequest(url="https://www.livecoin.net/en", callback=self.parse, endpoint="execute", args={
+            'lua-source': self.script
+        })
 
     def parse(self, response):
         pass
