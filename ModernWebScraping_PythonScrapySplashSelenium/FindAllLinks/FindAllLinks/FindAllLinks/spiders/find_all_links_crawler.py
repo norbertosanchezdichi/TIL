d8c604a36ccf65f35b38ccac27a68d2928641fe6
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-04-17 21:59:49 -0700

    Use global link_counter variable

--- ModernWebScraping_PythonScrapySplashSelenium/FindAllLinks/FindAllLinks/FindAllLinks/spiders/find_all_links_crawler.py
+++ ModernWebScraping_PythonScrapySplashSelenium/FindAllLinks/FindAllLinks/FindAllLinks/spiders/find_all_links_crawler.py
@@ -36,8 +36,8 @@ class FindAllLinksCrawlerSpider(CrawlSpider):
         links = response.xpath('//a')
         origin_url = response.url
         for link in links:
-            link_counter += 1
-            if link_counter > 500:
+            self.link_counter += 1
+            if self.link_counter > 500:
                 break
             
             if link.xpath('.//img').get():
