555fde9518ddf03cae95fe9694f9ed25a444e5c6
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-04-18 00:51:07 -0700

    Final attempt to make Splash compatible with Crawl template

--- ModernWebScraping_PythonScrapySplashSelenium/FindAllLinks/FindAllLinks/FindAllLinks/spiders/find_all_links_crawler.py
+++ ModernWebScraping_PythonScrapySplashSelenium/FindAllLinks/FindAllLinks/FindAllLinks/spiders/find_all_links_crawler.py
@@ -23,15 +23,50 @@ class FindAllLinksCrawlerSpider(CrawlSpider):
     '''
     
     rules = (
-        Rule(LinkExtractor(restrict_xpaths='//a'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(restrict_xpaths='//a'), callback='parse_item', follow=True, process_request='use_splash'),
     )
     
+    def use_splash(self, request):
+        request.meta.update(splash={
+          'args': {
+              'wait': 1,
+          },
+          'endpoint': 'render.html',           
+        })
+        return request
+    
     def start_requests(self):
-        yield SplashRequest(url='https://www.maximintegrated.com/en', callback=self.parse, endpoint='execute', args={'lua_source': self.script})
-       
+        url = 'https://www.maximintegrated.com/en'
+        yield SplashRequest(url=url, callback=self.parse_m, endpoint='execute', dont_filter=True,args={
+                'url': url, 'lua_source': self.script
+            })
+
+    def _requests_to_follow(self, response):
+        if not isinstance(response, (HtmlResponse, SplashJsonResponse, SplashTextResponse)):
+            return
+        seen = set()
+        for n, rule in enumerate(self._rules):
+            links = [lnk for lnk in rule.link_extractor.extract_links(response)
+                     if lnk not in seen]
+            if links and rule.process_links:
+                links = rule.process_links(links)
+            for link in links:
+                seen.add(link)
+                r = self._build_request(n, link)
+                yield rule.process_request(r)
+
+    def _build_request(self, rule, link):
+        r = SplashRequest(url=link.url, callback=self._response_downloaded, meta={'rule': rule, 'link_text': link.text},
+                          args={'url': link.url, 'lua_source': self.script})
+        r.meta.update(rule=rule, link_text=link.text)
+        return r
+            
     def parse_item(self, response):
         yield {
             'link_url': response.url,
             'link_title': response.xpath('//title/text()').get(),
             'HTTP status code': response.status
-        }    
+        }
+            
+            
+#yield scrapy.Request("http://localhost:8050/render.html?url=" + page_url, self.parse_page)
