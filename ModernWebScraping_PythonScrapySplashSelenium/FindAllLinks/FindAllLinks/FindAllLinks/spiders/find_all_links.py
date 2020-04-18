36fa2cbc6112f46262839ef07fe6290a43a5b213
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-04-17 22:28:23 -0700

    Copy parse function code from find_all_links_crawler

--- ModernWebScraping_PythonScrapySplashSelenium/FindAllLinks/FindAllLinks/FindAllLinks/spiders/find_all_links.py
+++ ModernWebScraping_PythonScrapySplashSelenium/FindAllLinks/FindAllLinks/FindAllLinks/spiders/find_all_links.py
@@ -18,28 +18,37 @@ class FindAllLinksSpider(scrapy.Spider):
             return splash:html()
         end
     '''
+    
+    link_counter = 0
 
     def start_requests(self):
         yield SplashRequest(url='https://www.maximintegrated.com', callback=self.parse, endpoint='execute', args={'lua_source': self.script})
 
     def parse(self, response):
         links = response.xpath('//a')
-        
+        origin_url = response.url
         for link in links:
+            self.link_counter += 1
+            if self.link_counter > 1000:
+                break
             
             if link.xpath('.//img').get():
                 link_text = link.xpath('.//img/@alt').get()
             else:
                 link_text = link.xpath('.//text()').get()
             
-            link_url = link.xpath('.//@href').get()
+            link_url = response.urljoin(link.xpath('.//@href').get())
+            if "javascript" not in link_url:
+                yield SplashRequest(url=link_url, callback=self.parse_item, endpoint='execute', args={'lua_source': self.script})
+            else:
+                continue
             
             yield {
-                    'link_text': link_text,
-                    'link_url': link_url,#response.urljoin(link_url),
-                    'parent_url': response.url,
-                    'HTTP status code': response.status
-            }
-            
+                'link_text': link_text,
+                'link_url': link_url,
+                'link_title': response.xpath('//title/text()').get(),
+                'HTTP status code': response.status,
+                'origin_url': origin_url
+            }    
         
 # if link_text has valid text (using regex. [a-zA-z\d]+)
