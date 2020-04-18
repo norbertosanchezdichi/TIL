50bb417fdf6e6becb0a064ad72bcb7d929eafc59
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-04-18 14:32:22 -0700

    Use SplashRequest.

--- ModernWebScraping_PythonScrapySplashSelenium/FindAllLinks/FindAllLinks/FindAllLinks/spiders/find_all_links.py
+++ ModernWebScraping_PythonScrapySplashSelenium/FindAllLinks/FindAllLinks/FindAllLinks/spiders/find_all_links.py
@@ -22,8 +22,7 @@ class FindAllLinksSpider(scrapy.Spider):
     '''
     
     def start_requests(self):
-        yield scrapy.Request("http://localhost:8050/render.html?url=" + self.allowed_domains[0], callback=self.parse_page)
-        #yield SplashRequest(url='https://www.maximintegrated.com/en', callback=self.parse_page, endpoint='execute', args={'lua_source': self.script})
+        yield SplashRequest(url='https://www.maximintegrated.com/en', callback=self.parse_page, endpoint='execute', args={'lua_source': self.script})
 
     def parse_page(self, response):
         origin_url = response.url
@@ -46,9 +45,7 @@ class FindAllLinksSpider(scrapy.Spider):
             if link_absolute_url not in self.links_crawled:
                 self.links_crawled.append(link_absolute_url)
                 try:
-                    yield scrapy.Request("http://localhost:8050/render.html?url=" + link_absolute_url)
-                    #yield SplashRequest(url=link_absolute_url, callback=self.parse_page, endpoint='execute', args={'lua_source': self.script})
-                    links_crawled
+                    yield SplashRequest(url=link_absolute_url, callback=self.parse_page, endpoint='execute', args={'lua_source': self.script})
                     link_title = response.xpath('//title/text()').get()
                     link_http_status = response.status
                 except:
--- ModernWebScraping_PythonScrapySplashSelenium/FindAllLinks/FindAllLinks/FindAllLinks/spiders/find_all_links_crawler.py
+++ ModernWebScraping_PythonScrapySplashSelenium/FindAllLinks/FindAllLinks/FindAllLinks/spiders/find_all_links_crawler.py
@@ -8,6 +8,8 @@ from scrapy_splash import SplashRequest
 class FindAllLinksCrawlerSpider(CrawlSpider):
     name = 'find_all_links_crawler'
     allowed_domains = ['www.maximintegrated.com/en']
+    links= {}
+    links_crawled = []
     
     script = '''
         function main(splash, args)
@@ -62,11 +64,42 @@ class FindAllLinksCrawlerSpider(CrawlSpider):
         return r
             
     def parse_item(self, response):
-        yield {
-            'link_url': response.url,
-            'link_title': response.xpath('//title/text()').get(),
-            'HTTP status code': response.status
-        }
+        origin_url = response.url
+        links = response.xpath('//a')
+        
+        for link in links:
+            link_relative_url = link.xpath('.//@href').get()
+            link_text = link.xpath('.//text()').get()
             
+            if not link_text:
+                link_text = link.xpath('.//img/@alt').get()
+            if not link_text:
+                link_text = link.xpath('.//img/@title').get()
+
+            self.links[link_relative_url] = link_text
+                
+        for link_relative_url, link_text in self.links.items():
+            link_absolute_url = response.urljoin(link_relative_url)
             
-#yield scrapy.Request("http://localhost:8050/render.html?url=" + page_url, self.parse_page)
+            if link_absolute_url not in self.links_crawled:
+                self.links_crawled.append(link_absolute_url)
+                try:
+                    yield SplashRequest(url=link_absolute_url, callback=self.parse_item, endpoint='execute', args={'lua_source': self.script})
+                    link_title = response.xpath('//title/text()').get()
+                    link_http_status = response.status
+                except:
+                    link_title = ''
+                    link_http_status = ''
+            else:
+                continue
+                
+            yield {
+                'link_text': link_text,
+                'link_relative_url': link_relative_url,
+                'link_absolute_url': link_absolute_url,
+                'link_title': link_title,
+                'HTTP status code': link_http_status,
+                'origin_url': origin_url
+            }
+            
+        print(f"links_crawled length: {len(self.links_crawled)}")
