3151e79c89f150967a2143a2612a75dc7a0cfe19
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-04-18 23:12:05 -0700

    Add examples for extract_all_phones(..) function

--- Python3/Bootcamp/RegularExpressions/phone.py
+++ Python3/Bootcamp/RegularExpressions/phone.py
@@ -7,12 +7,15 @@ def extra_phone(input):
         return match.group()
     return None
 
+print(extract_phone("my number is 323 240-4325"))
+print(extract_phone("my number is 323 240-43224"))
+print(extract_phone("323 240-4322"))
+
 def extra_all_phone(input):
     phone_regex = re.compile(r'\b\d{3} \d{3}-\d{4}\b')
     return phone_regex.find_all(input)
     
-print(extract_phone("my number is 323 240-4325"))
-print(extract_phone("my number is 323 240-43224"))
-print(extract_phone("323 240-4322"))
+print(extract_all_phone("my number is 323 240-4325 or call me here 423 432-4321"))
+print(extract_all_phone("my number is 323 240-43224"))
 
 #def is_valid_phone():
