73678d213b9779af3db5265570fd489838aab5fb
Norberto Sánchez-Dichi <norbertosanchezdichi@users.noreply.github.com>
2020-04-18 23:02:39 -0700

    Remove whitespace in filename

--- Python3/Bootcamp/RegularExpressions/intro.py
+++ Python3/Bootcamp/RegularExpressions/intro.py
@@ -11,5 +11,5 @@ print(res.group())
 res = pattern.findall("call me at 310 445-9876 432 432-5433")
 print(res)
 
-res = re.search(r'\d{3} \d{3}-\d{4}', "call me at 310 445-9876").group()
+res = re.search(r'\d{3} \d{3}-\d{4}', "call me at 310 445-9876").group(
 print(res)
