da5045af814455ab816159cea6de0ba5d5afe24d
Norberto Sánchez-Dichi <norbertosanchezdichi@gmail.com>
2021-01-01 23:48:44 -0800

    Add "Import dataset" section

--- Machine Learning A-Z/Data Pre-Processing/data_pre-processing_tools.py
+++ Machine Learning A-Z/Data Pre-Processing/data_pre-processing_tools.py
@@ -1,3 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+# Import dataset
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, -1].values
+
+print(X)
+print(Y)
