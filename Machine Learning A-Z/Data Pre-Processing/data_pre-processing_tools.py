5154bbc3b0000462d98f014efadc65dc20bda81c
Norberto Sánchez-Dichi <norbertosanchezdichi@gmail.com>
2021-01-02 00:09:03 -0800

    Add "Imputation" section to replace missing independent values using a simple mean.

--- Machine Learning A-Z/Data Pre-Processing/data_pre-processing_tools.py
+++ Machine Learning A-Z/Data Pre-Processing/data_pre-processing_tools.py
@@ -10,3 +10,11 @@ Y = dataset.iloc[:, -1].values
 
 print(f"X = {X}")
 print(f"Y = {Y}")
+
+# Imputation
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values = np.nan, strategy = 'mean')
+imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+print(f"X after imputation = {X}")
