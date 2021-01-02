448e00b11a39c50642308cde7bfcf8e6189a4f34
Norberto Sánchez-Dichi <norbertosanchezdichi@gmail.com>
2021-01-02 01:56:51 -0800

    Setup simple linear regression dataset.

---
+++ Machine Learning A-Z/Simple Linear Regression/simple_linear_regression.py
@@ -0,0 +1,23 @@
+# Import libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Import dataset
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, -1].values
+
+print(f"X = {X}")
+print(f"Y = {Y}")
+print()
+
+# Splitting Dataset: Training Set and Test Set
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 1)
+
+print(f"X_train = {X_train}")
+print(f"X_test = {X_test}")
+print(f"Y_train = {Y_train}")
+print(f"Y_test = {Y_test}")
+print()
