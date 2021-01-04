01835d3ef6296f0c08b6799f7dd519f6db5d3040
Norberto Sánchez-Dichi <norbertosanchezdichi@gmail.com>
2021-01-03 22:31:44 -0800

    Edit dataset to only include one feature in the training set

--- Machine Learning A-Z/Polynomial Regression/polynomial_regression.py
+++ Machine Learning A-Z/Polynomial Regression/polynomial_regression.py
@@ -5,9 +5,17 @@ import pandas as pd
 
 # Import dataset
 dataset = pd.read_csv('Position_Salaries.csv')
-X = dataset.iloc[:, 1:-1].values
+X = dataset.iloc[:, :-1].values
 Y = dataset.iloc[:, -1].values
 
 print(f"X = {X}")
 print(f"Y = {Y}")
+print()
+
+# Split Dataset: Training Set and Test Set
+X_train = X[:, 1:2]
+Y_train = Y
+
+print(f"X_train = {X_train}")
+print(f"Y_train = {Y_train}")
 print()
