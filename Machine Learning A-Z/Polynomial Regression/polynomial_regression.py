da26435cb6c76e8a1454c1d59ea0acd263885b5e
Norberto Sánchez-Dichi <norbertosanchezdichi@gmail.com>
2021-01-03 22:24:48 -0800

    Initial commit

--- Machine Learning A-Z/Polynomial Regression/polynomial_regression.py
+++ Machine Learning A-Z/Polynomial Regression/polynomial_regression.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 # Import dataset
 dataset = pd.read_csv('Position_Salaries.csv')
-X = dataset.iloc[:, :-1].values
+X = dataset.iloc[:, 1:-1].values
 Y = dataset.iloc[:, -1].values
 
 print(f"X = {X}")
