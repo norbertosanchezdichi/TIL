b26522319d94735e667583ee42ecac401fbc0952
Norberto Sánchez-Dichi <norbertosanchezdichi@gmail.com>
2021-01-03 18:01:59 -0800

    Initial commit

---
+++ Machine Learning A-Z/Polynomial Regression/polynomial_regression.py
@@ -0,0 +1,13 @@
+# Import libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Import dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, -1].values
+
+print(f"X = {X}")
+print(f"Y = {Y}")
+print()
