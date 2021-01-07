1ff912de76bbdaa5b0b3c9c2fc9d539312a5ff84
Norberto Sánchez-Dichi <norbertosanchezdichi@gmail.com>
2021-01-06 19:35:49 -0800

    Create 2D array for Y_train

--- MachineLearning/Regression/SupportVectorRegression(SVR)/support_vector_regression.py
+++ MachineLearning/Regression/SupportVectorRegression(SVR)/support_vector_regression.py
@@ -19,3 +19,9 @@ Y_train = Y
 print(f"X_train = {X_train}")
 print(f"Y_train = {Y_train}")
 print()
+
+# Convert Y_train to 2D Array
+Y_train = Y_train.reshape(len(Y_train), 1)
+
+print(f"Y_train as a 2D array = {Y_train}")
+print()
