8c9fe9f277ca9d3db2b0206538491188fa1066e3
Norberto Sánchez-Dichi <norbertosanchezdichi@gmail.com>
2021-01-16 21:12:20 -0800

    Use Pascal case for folder names

---
+++ MachineLearning/Classification/KNearestNeighbors(K-NN)/k_nearest_neighbors_(k-nn).py
@@ -0,0 +1,102 @@
+# Import libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Import dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, -1].values
+
+print(f"X = {X}")
+print(f"Y = {Y}")
+print()
+
+# Split Dataset: Training Set and Test Set
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.25, random_state = 0)
+
+print(f"X_train = {X_train}")
+print(f"X_test = {X_test}")
+print(f"Y_train = {Y_train}")
+print(f"Y_test = {Y_test}")
+print()
+
+# Feature Scaling (done after splitting to avoid information leakage.)
+## Not required for Logistic Regression, however, it improves training performance and final predictions.
+## Not done for dependent variable Y because the Logistic Regression model used is binary.
+from sklearn.preprocessing import StandardScaler
+standardScaler = StandardScaler()
+X_train_scaled = standardScaler.fit_transform(X_train)
+X_test_scaled = standardScaler.transform(X_test)
+
+print(f"X_train_scaled = {X_train_scaled}")
+print(f"X_test_scaled = {X_test_scaled}")
+print()
+
+# K Nearest Neighbors (K-NN)
+# A non-linear model.  
+# When a data point has an equal number of neighbors, one category is randomly chosen.
+## 1. Choose the number K of neighbors (commonly 5).
+## 2. Take the K nearest neighbors of data point according to the Euclidean distance.
+## 3. Among these K neighbors, count the number of data points in each category.
+## 4. Assign the new data point to the category where you counted the most neighbors.
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski', p = 2)
+classifier.fit(X_train_scaled, Y_train)
+
+# Predict if-purchase for 30 year old customer earning $87,000
+Y_predict = classifier.predict(standardScaler.transform([[30, 87000]]))
+
+# Output prediction salary for a position 6
+print(f"Purchase possible from 30 year old earning $87,000? = {Y_predict}.")
+print()
+
+# Predicting using Logistic Regression
+Y_predict = classifier.predict(X_test_scaled)
+print(f"[Y_predict Y_test] = {np.concatenate((Y_predict.reshape(len(Y_predict), 1), Y_test.reshape(len(Y_test), 1)), axis = 1)}")
+print()
+
+# Create Confusion Matrix
+## Not the optimal method to evaluate the performanc eof the model - K-Fold Cross Validation is preferred and it involves using validation tests.
+from sklearn.metrics import confusion_matrix
+print(f"Confusion Matrix = {confusion_matrix(Y_test, Y_predict)}")
+print()
+
+# Generate Accuracy Score
+from sklearn.metrics import accuracy_score
+print(f"Accuracy Score = {accuracy_score(Y_test, Y_predict)}")
+
+# Output Training Set Results
+from matplotlib.colors import ListedColormap
+X1, X2 = np.meshgrid(np.arange(start = X_train[:, 0].min() - 10, stop = X_train[:, 0].max() + 10, step = 0.25),
+                     np.arange(start = X_train[:, 1].min() - 1000, stop = X_train[:, 1].max() + 1000, step = 0.25))
+plt.contourf(X1, X2, classifier.predict(standardScaler.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(Y_train)):
+    plt.scatter(X_train[Y_train == j, 0], X_train[Y_train == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Logistic Regression (Training Set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.savefig('Logistic_Regression_Training_Set_Results.png')
+plt.clf()
+
+# Output Test Set Results
+from matplotlib.colors import ListedColormap
+X1, X2 = np.meshgrid(np.arange(start = X_test[:, 0].min() - 10, stop = X_test[:, 0].max() + 10, step = 0.25),
+                     np.arange(start = X_test[:, 1].min() - 1000, stop = X_test[:, 1].max() + 1000, step = 0.25))
+plt.contourf(X1, X2, classifier.predict(standardScaler.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(Y_test)):
+    plt.scatter(X_test[Y_test == j, 0], X_test[Y_test == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Logistic Regression (Test Set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.savefig('Logistic_Regression_Test_Set_Results.png')
+plt.clf()
---
+++ MachineLearning/Classification/LogisticRegression/logistic_regression.py
@@ -0,0 +1,104 @@
+# Import libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Import dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, -1].values
+
+print(f"X = {X}")
+print(f"Y = {Y}")
+print()
+
+# Split Dataset: Training Set and Test Set
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.25, random_state = 0)
+
+print(f"X_train = {X_train}")
+print(f"X_test = {X_test}")
+print(f"Y_train = {Y_train}")
+print(f"Y_test = {Y_test}")
+print()
+
+# Feature Scaling (done after splitting to avoid information leakage.)
+## Not required for Logistic Regression, however, it improves training performance and final predictions.
+## Not done for dependent variable Y because the Logistic Regression model used is binary.
+from sklearn.preprocessing import StandardScaler
+standardScaler = StandardScaler()
+X_train_scaled = standardScaler.fit_transform(X_train)
+X_test_scaled = standardScaler.transform(X_test)
+
+print(f"X_train_scaled = {X_train_scaled}")
+print(f"X_test_scaled = {X_test_scaled}")
+print()
+
+# Logistic Regression
+## Using the Sigmoid Function with Euler's number, solving for the dependent variable 'Y' in terms of the probability allows to model the solution with a linear equation.
+## The solution is in terms of probability using the natural log - hence why it is called Logistic Regression.
+# Assumptions when using Logistic Regression
+## 1. Binary Logistic Regreesion requires the dependent variable to be binary. Ordinal Logistic Regression requires the dependent variable to be ordinal.
+## 2. Requires the observations to be independent of each other.
+## 3. Requires little or no multicollinearity among the independent variables.
+## 4. Requires that the independent variables are linearly related to the log odds.
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train_scaled, Y_train)
+
+# Predict if-purchase for 30 year old customer earning $87,000
+Y_predict = classifier.predict(standardScaler.transform([[30, 87000]]))
+Y_predict_probability = classifier.predict_proba(standardScaler.transform([[30, 87000]]))
+
+# Output prediction salary for a position 6
+print(f"Purchase possible from 30 year old earning $87,000? = {Y_predict}.  What is the probability? = {Y_predict_probability}")
+print()
+
+# Predicting using Logistic Regression
+Y_predict = classifier.predict(X_test_scaled)
+print(f"[Y_predict Y_test] = {np.concatenate((Y_predict.reshape(len(Y_predict), 1), Y_test.reshape(len(Y_test), 1)), axis = 1)}")
+print()
+
+# Create Confusion Matrix
+## Not the optimal method to evaluate the performanc eof the model - K-Fold Cross Validation is preferred and it involves using validation tests.
+from sklearn.metrics import confusion_matrix
+print(f"Confusion Matrix = {confusion_matrix(Y_test, Y_predict)}")
+print()
+
+# Generate Accuracy Score
+from sklearn.metrics import accuracy_score
+print(f"Accuracy Score = {accuracy_score(Y_test, Y_predict)}")
+
+# Output Training Set Results
+from matplotlib.colors import ListedColormap
+X1, X2 = np.meshgrid(np.arange(start = X_train[:, 0].min() - 10, stop = X_train[:, 0].max() + 10, step = 0.25),
+                     np.arange(start = X_train[:, 1].min() - 1000, stop = X_train[:, 1].max() + 1000, step = 0.25))
+plt.contourf(X1, X2, classifier.predict(standardScaler.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(Y_train)):
+    plt.scatter(X_train[Y_train == j, 0], X_train[Y_train == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Logistic Regression (Training Set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.savefig('Logistic_Regression_Training_Set_Results.png')
+plt.clf()
+
+# Output Test Set Results
+from matplotlib.colors import ListedColormap
+X1, X2 = np.meshgrid(np.arange(start = X_test[:, 0].min() - 10, stop = X_test[:, 0].max() + 10, step = 0.25),
+                     np.arange(start = X_test[:, 1].min() - 1000, stop = X_test[:, 1].max() + 1000, step = 0.25))
+plt.contourf(X1, X2, classifier.predict(standardScaler.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(Y_test)):
+    plt.scatter(X_test[Y_test == j, 0], X_test[Y_test == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Logistic Regression (Test Set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.savefig('Logistic_Regression_Test_Set_Results.png')
+plt.clf()
