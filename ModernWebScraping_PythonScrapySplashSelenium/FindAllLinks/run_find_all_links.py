6fa5c23d07839724a267a2b2bb485d171d873863
Norberto Sánchez-Dichi <norbertosanchezdichi@gmail.com>
2021-01-16 21:15:41 -0800

    Edit folder name

--- ModernWebScraping_PythonScrapySplashSelenium/FindAllLinks/run_find_all_links.py
+++
@@ -1,9 +0,0 @@
-import scrapy
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
-from find_all_links.spiders.contries import FindAllLinksSpider
-
-
-process = CrawlerProcess(settings=get_project_settings())
-process.crawl(FindAllLinksSpider)
-process.start()
--- ModernWebScraping_PythonScrapySplashSelenium/SplashCrashCourse/intro.lua
+++
@@ -1,9 +0,0 @@
-function main(splash, args)
-	url = args.url
-  assert(splash:go(url))
-  assert(splash:wait(1))
-  return {
-    image = splash:png(),
-    html = splash:html()
-  }
-end
