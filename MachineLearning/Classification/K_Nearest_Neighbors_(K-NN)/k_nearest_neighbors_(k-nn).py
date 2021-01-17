06d2e9b5157b25416adfd6c9e26c5ea79845c472
Norberto Sánchez-Dichi <norbertosanchezdichi@gmail.com>
2021-01-16 21:10:21 -0800

    Add clarification to K Nearest Neighbors (K-NN) description

--- MachineLearning/Classification/K_Nearest_Neighbors_(K-NN)/k_nearest_neighbors_(k-nn).py
+++ MachineLearning/Classification/K_Nearest_Neighbors_(K-NN)/k_nearest_neighbors_(k-nn).py
@@ -35,6 +35,8 @@ print(f"X_test_scaled = {X_test_scaled}")
 print()
 
 # K Nearest Neighbors (K-NN)
+# A non-linear model.  
+# When a data point has an equal number of neighbors, one category is randomly chosen.
 ## 1. Choose the number K of neighbors (commonly 5).
 ## 2. Take the K nearest neighbors of data point according to the Euclidean distance.
 ## 3. Among these K neighbors, count the number of data points in each category.
