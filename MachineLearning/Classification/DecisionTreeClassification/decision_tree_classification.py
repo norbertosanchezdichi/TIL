28031383f8d19ce147612e6a3032bf1aa66613de
Norberto Sánchez-Dichi <norbertosanchezdichi@gmail.com>
2021-01-20 20:32:25 -0800

    Copy from Naïve-Bayes file.  Initial commit.

---
+++ MachineLearning/Classification/DecisionTreeClassification/decision_tree_classification.py
@@ -0,0 +1,43 @@
+# Import libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Import dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, -1].values
+
+print(f"X = {X}")
+print(f"Y = {Y}")
+print()
+
+# Split Dataset: Training Set and Test Set
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.25, random_state = 0)
+
+print(f"X_train = {X_train}")
+print(f"X_test = {X_test}")
+print(f"Y_train = {Y_train}")
+print(f"Y_test = {Y_test}")
+print()
+
+# Feature Scaling (done after splitting to avoid information leakage.)
+## Not required for Logistic Regression, however, it improves training performance and final predictions.
+## Not done for dependent variable Y because the Logistic Regression model used is binary.
+from sklearn.preprocessing import StandardScaler
+standardScaler = StandardScaler()
+X_train_scaled = standardScaler.fit_transform(X_train)
+X_test_scaled = standardScaler.transform(X_test)
+
+print(f"X_train_scaled = {X_train_scaled}")
+print(f"X_test_scaled = {X_test_scaled}")
+print()
+
+# Decision Tree Classifiier
+## Splits the dataset into homogenous classes with the goal to obtain the minimum entropy in each terminal leaf.
+### Uses reduction of standard deviation of the predictions.
+### Building a decision tree is all about finding the attribute that returns the highest standard deviation reduction.
+### The Information Gain is the reduction in standard deviation.
+## Not useful on its own but are incorporated in other methods which makes the overall model powerful.
+### Incorporated in Random Forest and used in Gradient Boosting.
