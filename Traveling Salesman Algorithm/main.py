4b77abb5b0ce775be3495501f8bd6254692bbd89
Norberto Sánchez-Dichi <norbertosanchezdichi@gmail.com>
2023-05-14 12:50:23 -0700

    Summarize code and alternatives.

--- Traveling Salesman Algorithm/main.py
+++ Traveling Salesman Algorithm/main.py
@@ -1,3 +1,17 @@
+"""
+The code solves the Traveling Salesman Problem using a brute-force approach by generating all possible permutations of the addresses and calculating the distance for each permutation. This 
+method involves examining every possible combination, which can be computationally expensive and 
+inefficient, especially as the number of addresses increases. Other approaches involving dynamic
+programming, heuristics, and other specialized algorithms that can give an exact answer are better
+suited for this problem.
+
+This code uses the Google Maps API to find the optimal route among a list of addresses. It 
+utilizes multiprocessing to speed up the calculations by concurrently processing multiple 
+permutations. The code also includes caching mechanisms to store geocoding results and distance 
+calculations, improving efficiency by avoiding redundant API calls.
+
+"""
+
 import googlemaps
 from itertools import permutations
 from multiprocessing import cpu_count
